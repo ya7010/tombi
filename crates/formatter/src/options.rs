c8834b0c08a61522839455fca169081597330b24
yassun7010 <yassun7010@outlook.com>
2024-10-09 15:52:19 +0900

    feat: add json schema.

--- crates/formatter/src/options.rs
+++ crates/formatter/src/options.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
 pub struct Options {
     pub line_length: u8,
 }
