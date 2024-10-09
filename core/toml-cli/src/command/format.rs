f7bc814f94d96a6580e0097913b71aab1280fe1f
yassun7010 <yassun7010@outlook.com>
2024-10-09 12:12:28 +0900

    feat: default line length.

--- core/toml-cli/src/command/format.rs
+++ core/toml-cli/src/command/format.rs
@@ -2,8 +2,8 @@
 #[derive(clap::Args)]
 pub struct Args {
     /// Set the line-length
-    #[arg(long)]
-    line_length: Option<u8>,
+    #[arg(long, default_value = "100")]
+    line_length: u8,
 }
 
 pub fn run(_args: Args) -> Result<(), crate::Error> {
