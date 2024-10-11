781f16de4e8db1f3cc106da4d468aaa961cc29b9
yassun7010 <yassun7010@outlook.com>
2024-10-11 21:39:23 +0900

    feat: rename type.

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -1,13 +1,27 @@
 mod error;
-mod lang;
 mod syntax_kind;
 
 pub use error::Error;
-pub use lang::TomlLang;
 pub use syntax_kind::SyntaxKind;
 
-pub type SyntaxNode = rowan::SyntaxNode<crate::TomlLang>;
-#[allow(unused)]
-pub type SyntaxToken = rowan::SyntaxToken<crate::TomlLang>;
-#[allow(unused)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TomlLanguage {}
+
+impl rowan::Language for TomlLanguage {
+    type Kind = crate::SyntaxKind;
+
+    fn kind_from_raw(raw: rowan::SyntaxKind) -> Self::Kind {
+        assert!(raw.0 <= crate::SyntaxKind::ROOT as u16);
+        unsafe { std::mem::transmute::<u16, crate::SyntaxKind>(raw.0) }
+    }
+    fn kind_to_raw(kind: Self::Kind) -> rowan::SyntaxKind {
+        kind.into()
+    }
+}
+
+pub type SyntaxNode = rowan::SyntaxNode<crate::TomlLanguage>;
+pub type SyntaxToken = rowan::SyntaxToken<crate::TomlLanguage>;
 pub type SyntaxElement = rowan::NodeOrToken<SyntaxNode, SyntaxToken>;
+pub type SyntaxNodeChildren = rowan::SyntaxNodeChildren<TomlLanguage>;
+pub type SyntaxElementChildren = rowan::SyntaxElementChildren<TomlLanguage>;
+pub type PreorderWithTokens = rowan::api::PreorderWithTokens<TomlLanguage>;
