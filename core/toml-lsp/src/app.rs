bd3c84651e6834c84368b64e923959b2a2073c01
yassun7010 <yassun7010@outlook.com>
2024-10-19 03:21:52 +0900

    feat: add tracing.

---
+++ core/toml-lsp/src/app.rs
@@ -0,0 +1,36 @@
+use clap::Parser;
+use clap_verbosity_flag::{InfoLevel, Verbosity};
+use tracing_subscriber::prelude::*;
+
+/// TOML: TOML linter and code formatter.
+#[derive(clap::Parser)]
+#[command(name = "toml", version)]
+pub struct Args {
+    #[command(flatten)]
+    verbose: Verbosity<InfoLevel>,
+}
+
+impl<I, T> From<I> for Args
+where
+    I: IntoIterator<Item = T>,
+    T: Into<std::ffi::OsString> + Clone,
+{
+    #[inline]
+    fn from(value: I) -> Self {
+        Self::parse_from(value)
+    }
+}
+
+pub fn run(args: impl Into<Args>) -> Result<(), anyhow::Error> {
+    let args: Args = args.into();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from(
+            args.verbose.log_level_filter().to_string(),
+        ))
+        .with(tracing_subscriber::fmt::layer().pretty().without_time())
+        .init();
+
+    tracing::info!("Starting toml-lsp");
+
+    Ok(())
+}
---
+++ core/toml-lsp/src/main.rs
@@ -0,0 +1,8 @@
+mod app;
+
+fn main() {
+    if let Err(err) = app::run(std::env::args_os()) {
+        eprintln!("Error: {}", err);
+        std::process::exit(1);
+    }
+}
