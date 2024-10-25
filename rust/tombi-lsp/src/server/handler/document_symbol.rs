1f7ac489d7afb33ba984c9a0b4c8a88cd00e1ceb
yassun7010 <yassun7010@outlook.com>
2024-10-25 23:01:14 +0900

    wip: add document symbol.

--- rust/tombi-lsp/src/server/handler/document_symbol.rs
+++ rust/tombi-lsp/src/server/handler/document_symbol.rs
@@ -1,10 +1,34 @@
-use tower_lsp::lsp_types::{DocumentSymbolParams, DocumentSymbolResponse};
+use tower_lsp::lsp_types::{
+    DocumentSymbolParams, DocumentSymbolResponse, Location, Position, Range, SymbolInformation,
+};
 
 use crate::server::backend::Backend;
 
 pub async fn handle_document_symbol(
     backend: &Backend,
-    _params: DocumentSymbolParams,
+    params: DocumentSymbolParams,
 ) -> Result<Option<DocumentSymbolResponse>, tower_lsp::jsonrpc::Error> {
-    Ok(Some(DocumentSymbolResponse::Flat(vec![])))
+    Ok(Some(DocumentSymbolResponse::Flat(vec![
+        #[allow(deprecated)]
+        SymbolInformation {
+            name: "members".to_string(),
+            kind: tower_lsp::lsp_types::SymbolKind::CLASS,
+            tags: None,
+            location: Location {
+                uri: params.text_document.uri,
+                range: Range {
+                    start: Position {
+                        line: 2,
+                        character: 0,
+                    },
+                    end: Position {
+                        line: 2,
+                        character: 20,
+                    },
+                },
+            },
+            container_name: Some("Workspace".to_string()),
+            deprecated: None,
+        },
+    ])))
 }
