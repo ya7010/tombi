0a38b92ae79d4935a80959a06377423f348e7561
yassun7010 <yassun7010@outlook.com>
2024-10-27 01:20:40 +0900

    feat: add formatter to lsp.

--- rust/tombi-lsp/src/server/handler/formatting.rs
+++ rust/tombi-lsp/src/server/handler/formatting.rs
@@ -6,19 +6,5 @@ pub async fn handle_formatting(
     backend: &Backend,
     params: DocumentFormattingParams,
 ) -> Result<Option<Vec<TextEdit>>, tower_lsp::jsonrpc::Error> {
-    // Ok(Some(vec![TextEdit {
-    //     range: Range {
-    //         start: Position {
-    //             line: 0,
-    //             character: 0,
-    //         },
-    //         end: Position {
-    //             line: 0,
-    //             character: 0,
-    //         },
-    //     },
-    //     new_text: "!!!!!!!!!!!!!!!!!!!".to_string(),
-    // }]))
-
     Ok(None)
 }
