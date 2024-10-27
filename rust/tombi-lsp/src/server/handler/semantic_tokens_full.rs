5ec62deaaac6645a6d758e04a07eaa6cae55e92c
yassun7010 <yassun7010@outlook.com>
2024-10-28 00:46:39 +0900

    feat: semantic tokens.

--- rust/tombi-lsp/src/server/handler/semantic_tokens_full.rs
+++ rust/tombi-lsp/src/server/handler/semantic_tokens_full.rs
@@ -1,7 +1,9 @@
 use ast::AstNode;
 use parser::SyntaxNode;
+use text_position::TextPosition;
 use tower_lsp::lsp_types::{
-    SemanticToken, SemanticTokenType, SemanticTokens, SemanticTokensParams, SemanticTokensResult,
+    Position, Range, SemanticToken, SemanticTokenType, SemanticTokens, SemanticTokensParams,
+    SemanticTokensResult,
 };
 
 use crate::{server::backend::Backend, toml};
@@ -31,7 +33,7 @@ impl TokenType {
 }
 
 pub async fn handle_semantic_tokens_full(
-    backend: &Backend,
+    _backend: &Backend,
     SemanticTokensParams { text_document, .. }: SemanticTokensParams,
 ) -> Result<Option<SemanticTokensResult>, tower_lsp::jsonrpc::Error> {
     let source = toml::try_load(&text_document.uri)?;
@@ -41,8 +43,9 @@ pub async fn handle_semantic_tokens_full(
         return Ok(None);
     };
 
-    let mut tokens = Vec::new();
-    ast.append_semantic_tokens(&mut tokens);
+    let mut tokens_builder = SemanticTokensBuilder::new(&source);
+    ast.append_semantic_tokens(&mut tokens_builder);
+    let tokens = tokens_builder.build();
 
     tracing::info!("SemanticTokens: {tokens:#?}");
 
@@ -53,130 +56,187 @@ pub async fn handle_semantic_tokens_full(
 }
 
 trait AppendSemanticTokens {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>);
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder);
 }
 
 impl AppendSemanticTokens for ast::Root {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         for item in self.items() {
-            item.append_semantic_tokens(tokens);
+            item.append_semantic_tokens(builder);
         }
     }
 }
 
 impl AppendSemanticTokens for ast::RootItem {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         match self {
-            Self::Table(table) => table.append_semantic_tokens(tokens),
-            Self::ArrayOfTable(array) => array.append_semantic_tokens(tokens),
-            Self::KeyValue(key_value) => key_value.append_semantic_tokens(tokens),
+            Self::Table(table) => table.append_semantic_tokens(builder),
+            Self::ArrayOfTable(array) => array.append_semantic_tokens(builder),
+            Self::KeyValue(key_value) => key_value.append_semantic_tokens(builder),
         }
     }
 }
 
 impl AppendSemanticTokens for ast::Table {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         if let Some(header) = self.header() {
             for key in header.keys() {
-                tokens.push(semantic_token(TokenType::STRUCT, key.syntax()));
+                builder.add_token(TokenType::STRUCT, key.syntax());
             }
         }
 
         for entry in self.key_values() {
-            entry.append_semantic_tokens(tokens);
+            entry.append_semantic_tokens(builder);
         }
     }
 }
 
 impl AppendSemanticTokens for ast::ArrayOfTable {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         if let Some(header) = self.header() {
             for key in header.keys() {
-                tokens.push(semantic_token(TokenType::STRUCT, key.syntax()));
+                builder.add_token(TokenType::STRUCT, key.syntax());
             }
         }
 
         for table in self.key_values() {
-            table.append_semantic_tokens(tokens);
+            table.append_semantic_tokens(builder);
         }
     }
 }
 
 impl AppendSemanticTokens for ast::KeyValue {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
-        self.keys().map(|key| key.append_semantic_tokens(tokens));
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
+        self.keys().map(|key| key.append_semantic_tokens(builder));
         self.value()
-            .map(|value| value.append_semantic_tokens(tokens));
+            .map(|value| value.append_semantic_tokens(builder));
     }
 }
 
 impl AppendSemanticTokens for ast::Keys {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         for key in self.keys() {
-            key.append_semantic_tokens(tokens);
+            key.append_semantic_tokens(builder);
         }
     }
 }
 
 impl AppendSemanticTokens for ast::Key {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
-        tokens.push(semantic_token(TokenType::VARIABLE, self.syntax()));
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
+        builder.add_token(TokenType::VARIABLE, self.syntax());
     }
 }
 
 impl AppendSemanticTokens for ast::Value {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         match self {
             Self::BasicString(_)
             | Self::LiteralString(_)
             | Self::MultiLineBasicString(_)
             | Self::MultiLineLiteralString(_) => {
-                tokens.push(semantic_token(TokenType::STRING, self.syntax()))
+                builder.add_token(TokenType::STRING, self.syntax())
             }
             Self::IntegerBin(_)
             | Self::IntegerOct(_)
             | Self::IntegerDec(_)
             | Self::IntegerHex(_)
-            | Self::Float(_) => tokens.push(semantic_token(TokenType::NUMBER, self.syntax())),
-            Self::Boolean(_) => tokens.push(semantic_token(TokenType::KEYWORD, self.syntax())),
+            | Self::Float(_) => builder.add_token(TokenType::NUMBER, self.syntax()),
+            Self::Boolean(_) => builder.add_token(TokenType::KEYWORD, self.syntax()),
             Self::OffsetDateTime(_)
             | Self::LocalDateTime(_)
             | Self::LocalDate(_)
-            | Self::LocalTime(_) => tokens.push(semantic_token(TokenType::REGEXP, self.syntax())),
-            Self::Array(array) => array.append_semantic_tokens(tokens),
-            Self::InlineTable(inline_table) => inline_table.append_semantic_tokens(tokens),
+            | Self::LocalTime(_) => builder.add_token(TokenType::REGEXP, self.syntax()),
+            Self::Array(array) => array.append_semantic_tokens(builder),
+            Self::InlineTable(inline_table) => inline_table.append_semantic_tokens(builder),
         }
     }
 }
 
 impl AppendSemanticTokens for ast::Array {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         for value in self.elements() {
-            value.append_semantic_tokens(tokens);
+            value.append_semantic_tokens(builder);
         }
     }
 }
 
 impl AppendSemanticTokens for ast::InlineTable {
-    fn append_semantic_tokens(&self, tokens: &mut Vec<SemanticToken>) {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
         for entry in self.elements() {
-            entry.append_semantic_tokens(tokens);
+            entry.append_semantic_tokens(builder);
         }
     }
 }
 
-fn semantic_token(token_type: TokenType, node: &SyntaxNode) -> SemanticToken {
-    SemanticToken {
-        delta_line: 0,
-        delta_start: node.text_range().start().into(),
-        length: node.text().len().into(),
-        token_type: token_type as u32,
-        token_modifiers_bitset: TokenType::LEGEND.iter().enumerate().fold(
-            0,
-            |mut total, (i, _)| {
-                total += 1 << i;
-                total
-            },
-        ),
+struct SemanticTokensBuilder<'a> {
+    tokens: Vec<SemanticToken>,
+    last_range: Range,
+    source: &'a str,
+}
+
+impl<'a> SemanticTokensBuilder<'a> {
+    fn new(source: &'a str) -> Self {
+        Self {
+            tokens: Vec::new(),
+            last_range: Range::default(),
+            source,
+        }
     }
+
+    fn add_token(&mut self, token_type: TokenType, node: &SyntaxNode) {
+        let range = Range::new(
+            TextPosition::from_source(self.source, node.text_range().start()).into(),
+            TextPosition::from_source(self.source, node.text_range().end()).into(),
+        );
+
+        let relative = relative_range(range, self.last_range);
+
+        #[allow(clippy::cast_possible_truncation)]
+        self.tokens.push(SemanticToken {
+            delta_line: relative.start.line as u32,
+            delta_start: relative.start.character as u32,
+            length: (relative.end.character - relative.start.character) as u32,
+            token_type: token_type as u32,
+            token_modifiers_bitset: 0,
+        });
+
+        self.last_range = range;
+    }
+
+    fn build(self) -> Vec<SemanticToken> {
+        self.tokens
+    }
+}
+
+pub fn relative_position(position: Position, to: Position) -> Position {
+    if position.line == to.line {
+        Position {
+            line: 0,
+            character: position.character - to.character,
+        }
+    } else {
+        Position {
+            line: position.line - to.line,
+            character: position.character,
+        }
+    }
+}
+
+pub fn relative_range(range: Range, to: Range) -> Range {
+    let line_diff = range.end.line - range.start.line;
+    let start = relative_position(range.start, to.start);
+
+    let end = if line_diff == 0 {
+        Position {
+            line: start.line,
+            character: start.character + range.end.character - range.start.character,
+        }
+    } else {
+        Position {
+            line: start.line + line_diff,
+            character: range.end.character,
+        }
+    };
+
+    Range { start, end }
 }
