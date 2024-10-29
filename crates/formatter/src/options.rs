0d267c79f98a8688dd34f7ab478df0e8d0b68c92
yassun7010 <yassun7010@outlook.com>
2024-10-29 20:59:44 +0900

    fix: formatter options.

--- crates/formatter/src/options.rs
+++ crates/formatter/src/options.rs
@@ -1,12 +1,4 @@
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
+#[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
 pub struct Options {
     pub max_line_length: Option<u8>,
-}
-
-impl Default for Options {
-    fn default() -> Self {
-        Self {
-            max_line_length: Default::default(),
-        }
-    }
 }
