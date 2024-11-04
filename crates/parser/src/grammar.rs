a340c04106c1ad1cd51a0163f91caf79935ec1ce
yassun7010 <yassun7010@outlook.com>
2024-11-05 00:04:08 +0900

    refactor: move support fn.

--- crates/parser/src/grammar.rs
+++ crates/parser/src/grammar.rs
@@ -10,7 +10,7 @@ mod value;
 use root::parse_root;
 
 use crate::output::Output;
-use crate::SyntaxKind::*;
+use support::*;
 
 pub fn parse(input: &crate::Input) -> Output {
     let _p = tracing::info_span!("grammar::parse").entered();
@@ -23,48 +23,52 @@ pub fn parse(input: &crate::Input) -> Output {
     crate::event::process(events)
 }
 
-fn begin_dangling_comments(p: &mut crate::parser::Parser<'_>) {
-    let mut n = 0;
-    let mut comment_count = 0;
-    while p.nth_at(n, WHITESPACE) || p.nth_at(n, COMMENT) || p.nth_at(n, NEWLINE) {
-        let kind = p.nth(n);
-        match kind {
-            COMMENT => {
-                comment_count += 1;
-            }
-            NEWLINE => {
-                if p.nth_at(n + 1, NEWLINE) {
-                    if comment_count > 0 {
-                        (0..=n).for_each(|_| p.bump_any());
-                        while p.eat(NEWLINE) || p.eat(WHITESPACE) {}
-                        break;
-                    }
-                    n += 1;
+mod support {
+    use crate::SyntaxKind::*;
+
+    pub fn begin_dangling_comments(p: &mut crate::parser::Parser<'_>) {
+        let mut n = 0;
+        let mut comment_count = 0;
+        while p.nth_at(n, WHITESPACE) || p.nth_at(n, COMMENT) || p.nth_at(n, NEWLINE) {
+            let kind = p.nth(n);
+            match kind {
+                COMMENT => {
+                    comment_count += 1;
                 }
+                NEWLINE => {
+                    if p.nth_at(n + 1, NEWLINE) {
+                        if comment_count > 0 {
+                            (0..=n).for_each(|_| p.bump_any());
+                            while p.eat(NEWLINE) || p.eat(WHITESPACE) {}
+                            break;
+                        }
+                        n += 1;
+                    }
+                }
+                _ => {}
             }
-            _ => {}
+            n += 1;
         }
-        n += 1;
-    }
-}
-
-fn end_dangling_comments(p: &mut crate::parser::Parser<'_>) {
-    while p.eat(WHITESPACE) || p.eat(COMMENT) || p.eat(NEWLINE) {}
-}
-
-fn peek_leading_comments(p: &mut crate::parser::Parser<'_>) -> usize {
-    let mut n = 0;
-    while p.nth_at(n, WHITESPACE) || p.nth_at(n, COMMENT) || p.nth_at(n, NEWLINE) {
-        n += 1;
     }
 
-    n
-}
+    pub fn end_dangling_comments(p: &mut crate::parser::Parser<'_>) {
+        while p.eat(WHITESPACE) || p.eat(COMMENT) || p.eat(NEWLINE) {}
+    }
 
-fn leading_comments(p: &mut crate::parser::Parser<'_>) {
-    while p.eat(WHITESPACE) || p.eat(COMMENT) || p.eat(NEWLINE) {}
-}
+    pub fn peek_leading_comments(p: &mut crate::parser::Parser<'_>) -> usize {
+        let mut n = 0;
+        while p.nth_at(n, WHITESPACE) || p.nth_at(n, COMMENT) || p.nth_at(n, NEWLINE) {
+            n += 1;
+        }
 
-fn tailing_comment(p: &mut crate::parser::Parser<'_>) {
-    while p.eat(WHITESPACE) || p.eat(COMMENT) {}
+        n
+    }
+
+    pub fn leading_comments(p: &mut crate::parser::Parser<'_>) {
+        while p.eat(WHITESPACE) || p.eat(COMMENT) || p.eat(NEWLINE) {}
+    }
+
+    pub fn tailing_comment(p: &mut crate::parser::Parser<'_>) {
+        while p.eat(WHITESPACE) || p.eat(COMMENT) {}
+    }
 }
