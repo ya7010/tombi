61f69143d4e6e89456f0f8e4c44a2e0a9145317a
yassun7010 <yassun7010@outlook.com>
2024-11-07 14:00:19 +0900

    remove: match_ast.

--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -288,30 +288,3 @@ impl InlineTable {
         support::has_inner_comments(self.syntax().children_with_tokens(), T!('{'), T!('}'))
     }
 }
-
-/// Matches a `SyntaxNode` against an `ast` type.
-///
-/// # Example:
-///
-/// ```ignore
-/// match_ast! {
-///     match node {
-///         ast::CallExpr(it) => { ... },
-///         ast::MethodCallExpr(it) => { ... },
-///         ast::MacroCall(it) => { ... },
-///         _ => None,
-///     }
-/// }
-/// ```
-#[macro_export]
-macro_rules! match_ast {
-    (match $node:ident { $($tt:tt)* }) => { $crate::match_ast!(match ($node) { $($tt)* }) };
-
-    (match ($node:expr) {
-        $( $( $path:ident )::+ ($it:pat) => $res:expr, )*
-        _ => $catch_all:expr $(,)?
-    }) => {{
-        $( if let Some($it) = $($path::)+cast($node.clone()) { $res } else )*
-        { $catch_all }
-    }};
-}
