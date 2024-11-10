d1523ed80ceb44611210a8197e77cfe3f7b274e3
yassun7010 <yassun7010@outlook.com>
2024-11-11 00:16:21 +0900

    test: add test case.

--- crates/parser/src/grammar/table.rs
+++ crates/parser/src/grammar/table.rs
@@ -67,6 +67,19 @@ key2 = 2
 key1 = 1
 key2 = 2
 "#.trim_start(), crate::Error::ExpectedBracketEnd, ((0, 5), (0, 8)))]
+    #[case(r#"
+[aaa.bbb]
+key1 = 1
+key2 = 2
+
+[aaa.ccc]
+key1 =
+key2 = 2
+
+[aaa.ddd]
+key1 = 1
+key2 = 2
+"#.trim_start(), crate::Error::ExpectedValue, ((5, 5), (5, 6)))]
     fn invalid_table(
         #[case] source: &str,
         #[case] error: crate::Error,
