2c5ae49b541d850091901895356147b3e517be1f
yassun7010 <yassun7010@outlook.com>
2024-11-18 01:49:01 +0900

    fix: compile error.

--- crates/text/src/lib.rs
+++ crates/text/src/lib.rs
@@ -15,4 +15,4 @@ pub use crate::{
 };
 
 #[cfg(target_pointer_width = "16")]
-compile_error!("text-size assumes usize >= u32 and does not work on 16-bit targets");
+compile_error!("'text' crate assumes usize >= u32 and does not work on 16-bit targets");
