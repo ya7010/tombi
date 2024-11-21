c4f6ac6cf5628ce549d6eb193304549228d95e6e
yassun7010 <yassun7010@outlook.com>
2024-11-21 21:41:53 +0900

    chore: add alias.

--- rust/tombi-cli/src/app/command.rs
+++ rust/tombi-cli/src/app/command.rs
@@ -3,6 +3,9 @@ pub mod lint;
 
 #[derive(clap::Subcommand)]
 pub enum TomlCommand {
+    #[command(alias = "fmt")]
     Format(format::Args),
+
+    #[command(alias = "check")]
     Lint(lint::Args),
 }
