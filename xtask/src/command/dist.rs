debaf215b6147d62cc5627d4a94c0c5b7032bbf8
yassun7010 <yassun7010@outlook.com>
2024-11-24 13:40:37 +0900

    wip: ci.

--- xtask/src/command/dist.rs
+++ xtask/src/command/dist.rs
@@ -1,8 +1,17 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fs::File,
+    io::{self, BufWriter},
+    path::{Path, PathBuf},
+};
 
+use flate2::{write::GzEncoder, Compression};
+use time::OffsetDateTime;
 use xshell::Shell;
+use zip::{write::FileOptions, DateTime, ZipWriter};
 
 use crate::utils::project_root;
+
+const DEV_VERSION: &str = "0.0.0";
 
 pub fn run(sh: &Shell) -> Result<(), anyhow::Error> {
     let project_root = project_root();
@@ -14,32 +23,117 @@ pub fn run(sh: &Shell) -> Result<(), anyhow::Error> {
     sh.remove_path(&dist)?;
     sh.create_dir(&dist)?;
 
-    dist_editor_vscode(sh, &target)?;
+    dist_server(sh, &target)?;
+    dist_client(sh, &target)?;
 
     Ok(())
 }
 
+fn dist_server(sh: &Shell, target: &Target) -> Result<(), anyhow::Error> {
+    let _e = sh.push_env("CARGO_PROFILE_RELEASE_LTO", "true");
+
+    let target_name = &target.name;
+
+    if target_name.contains("-linux-") {
+        std::env::set_var("CC", "clang");
+    }
+
+    let manifest_path = project_root()
+        .join("rust")
+        .join("tombi-cli")
+        .join("Cargo.toml");
+
+    xshell::cmd!(
+        sh,
+        "cargo build --manifest-path {manifest_path} --bin tombi --target {target_name} --release"
+    )
+    .run()?;
+
+    let dist = project_root().join("dist").join(&target.artifact_name);
+    gzip(&target.server_path, &dist.with_extension("gz"))?;
+    if target_name.contains("-windows-") {
+        zip(
+            &target.server_path,
+            target.symbols_path.as_ref(),
+            &dist.with_extension("zip"),
+        )?;
+    }
+
+    Ok(())
+}
+
+fn gzip(src_path: &Path, dest_path: &Path) -> anyhow::Result<()> {
+    let mut encoder = GzEncoder::new(File::create(dest_path)?, Compression::best());
+    let mut input = std::io::BufReader::new(File::open(src_path)?);
+    std::io::copy(&mut input, &mut encoder)?;
+    encoder.finish()?;
+    Ok(())
+}
+
+fn zip(src_path: &Path, symbols_path: Option<&PathBuf>, dest_path: &Path) -> anyhow::Result<()> {
+    let file = File::create(dest_path)?;
+    let mut writer = ZipWriter::new(BufWriter::new(file));
+    writer.start_file(
+        src_path.file_name().unwrap().to_str().unwrap(),
+        FileOptions::<()>::default()
+            .last_modified_time(
+                DateTime::try_from(OffsetDateTime::from(
+                    std::fs::metadata(src_path)?.modified()?,
+                ))
+                .unwrap(),
+            )
+            .unix_permissions(0o755)
+            .compression_method(zip::CompressionMethod::Deflated)
+            .compression_level(Some(9)),
+    )?;
+    let mut input = io::BufReader::new(File::open(src_path)?);
+    io::copy(&mut input, &mut writer)?;
+    if let Some(symbols_path) = symbols_path {
+        writer.start_file(
+            symbols_path.file_name().unwrap().to_str().unwrap(),
+            FileOptions::<()>::default()
+                .last_modified_time(
+                    DateTime::try_from(OffsetDateTime::from(
+                        std::fs::metadata(src_path)?.modified()?,
+                    ))
+                    .unwrap(),
+                )
+                .compression_method(zip::CompressionMethod::Deflated)
+                .compression_level(Some(9)),
+        )?;
+        let mut input = io::BufReader::new(File::open(symbols_path)?);
+        io::copy(&mut input, &mut writer)?;
+    }
+    writer.finish()?;
+    Ok(())
+}
+
+fn dist_client(sh: &Shell, target: &Target) -> Result<(), anyhow::Error> {
+    dist_editor_vscode(sh, target)
+}
+
 fn dist_editor_vscode(sh: &Shell, target: &Target) -> Result<(), anyhow::Error> {
-    let bundle_path = project_root().join("editors").join("vscode").join("server");
+    let vscode_path = project_root().join("editors").join("vscode");
+    let bundle_path = vscode_path.join("server");
     sh.remove_path(&bundle_path)?;
     sh.create_dir(&bundle_path)?;
 
-    if !target.cli_path.exists() {
+    if !target.server_path.exists() {
         return Err(anyhow::anyhow!(
             "CLI binary not found at {}. Please run `cargo build --package tombi-cli --release` first.",
-            target.cli_path.display()
+            target.server_path.display()
         ));
     }
 
-    sh.copy_file(&target.cli_path, &bundle_path.join("tombi"))?;
+    sh.copy_file(&target.server_path, &bundle_path.join("tombi"))?;
     if let Some(symbols_path) = &target.symbols_path {
         sh.copy_file(symbols_path, &bundle_path)?;
     }
 
-    let _d = sh.push_dir("./editors/vscode");
-    let mut patch = Patch::new(sh, "./package.json")?;
+    let _d: xshell::PushDir<'_> = sh.push_dir(vscode_path);
+    let mut patch = Patch::new(sh, "package.json")?;
     patch.replace(
-        &format!(r#""version": "0.0.0""#),
+        &format!(r#""version": "{}""#, DEV_VERSION),
         &format!(r#""version": "{}""#, target.version),
     );
     patch.commit(sh)?;
@@ -51,7 +145,7 @@ fn dist_editor_vscode(sh: &Shell, target: &Target) -> Result<(), anyhow::Error> 
 struct Target {
     name: String,
     version: String,
-    cli_path: PathBuf,
+    server_path: PathBuf,
     symbols_path: Option<PathBuf>,
     artifact_name: String,
 }
@@ -66,7 +160,7 @@ impl Target {
                 } else if cfg!(target_os = "windows") {
                     "x86_64-pc-windows-msvc".to_owned()
                 } else if cfg!(target_os = "macos") {
-                    "x86_64-apple-darwin".to_owned()
+                    "aarch64-apple-darwin".to_owned()
                 } else {
                     panic!("Unsupported OS, maybe try setting TOMBI_TARGET")
                 }
@@ -76,21 +170,21 @@ impl Target {
             Ok(github_ref) if github_ref.starts_with("refs/tags/v") => {
                 github_ref.trim_start_matches("refs/tags/v").to_owned()
             }
-            _ => "0.0.0".to_owned(),
+            _ => DEV_VERSION.to_owned(),
         };
-        let out_path = project_root.join("target").join("release");
+        let out_path = project_root.join("target").join(&name).join("release");
         let (exe_suffix, symbols_path) = if name.contains("-windows-") {
             (".exe".into(), Some(out_path.join("tombi.pdb")))
         } else {
             (String::new(), None)
         };
-        let cli_path = out_path.join(format!("tombi{exe_suffix}"));
+        let server_path = out_path.join(format!("tombi{exe_suffix}"));
         let artifact_name = format!("tombi-{name}{exe_suffix}");
 
         Self {
             name,
             version,
-            cli_path,
+            server_path,
             symbols_path,
             artifact_name,
         }
