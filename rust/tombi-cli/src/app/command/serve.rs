d134627c51b3419b797e6c67d21603c726d83255
yassun7010 <yassun7010@outlook.com>
2024-11-24 22:36:17 +0900

    feat: move toml-lsp to crates.

--- rust/tombi-cli/src/app/command/serve.rs
+++ rust/tombi-cli/src/app/command/serve.rs
@@ -1,10 +1,10 @@
-pub use tombi_lsp::server::Args;
+pub use lsp::server::Args;
 
 pub fn run(args: impl Into<Args>) -> Result<(), crate::Error> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
-        .block_on(tombi_lsp::server::run(args))?;
+        .block_on(lsp::server::run(args))?;
 
     Ok(())
 }
