df86885867e14660d56ffd8ea25dd613c00e2a9d
yassun7010 <yassun7010@outlook.com>
2024-11-24 20:37:18 +0900

    feat: ci.

---
+++ python/set_version.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+project_root = Path(__file__).parents[1].absolute()
+
+pyproject_toml_path = project_root / "pyproject.toml"
+
+with open(pyproject_toml_path, "r") as f:
+    contents = f.read()
+
+with open(pyproject_toml_path, "w") as f:
+    if (github_ref := os.getenv("GITHUB_REF")) and github_ref.startswith("refs/tags/"):
+        version = github_ref.replace("refs/tags/v", "")
+        contents = contents.replace('version = "0.0.0"', f'version = "{version}"')
+
+    f.write(contents)
