d9537bf539e88df9283e8b0c9d7667ffdf1f4f59
yassun7010 <yassun7010@outlook.com>
2024-11-24 22:45:26 +0900

    refactor: remove unuse document.

--- crates/lsp/src/document.rs
+++ crates/lsp/src/document.rs
@@ -1,15 +1,11 @@
 #[derive(Debug, Clone)]
 pub struct Document {
     pub source: String,
-    pub document: Option<document::Table>,
 }
 
 impl Document {
     pub fn new(source: impl Into<String>) -> Self {
         let source = source.into();
-        Self {
-            source,
-            document: None,
-        }
+        Self { source }
     }
 }
