08d8d96bd5b529d78bfde7de0c81a101ef6d6e92
yassun7010 <yassun7010@outlook.com>
2024-11-26 23:14:31 +0900

    feat: jsonschema bin.

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -3,6 +3,9 @@ pub mod lint;
 mod toml_version;
 pub use toml_version::TomlVersion;
 
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
+#[derive(Debug, Default, Clone)]
 pub struct Config {
     pub toml_version: TomlVersion,
     pub format: format::Options,
