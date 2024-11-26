93752589be6153bfc9868a97fe07561466519414
yassun7010 <yassun7010@outlook.com>
2024-11-26 23:27:12 +0900

    refactor: use cfg_attr.

--- crates/config/src/format/line_ending.rs
+++ crates/config/src/format/line_ending.rs
@@ -1,35 +1,11 @@
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
 #[derive(Debug, Default, Clone, Copy)]
 pub enum LineEnding {
     #[default]
+    #[cfg_attr(feature = "serde", serde(rename = "lf"))]
     Lf,
+
+    #[cfg_attr(feature = "serde", serde(rename = "crlf"))]
     Crlf,
-}
-
-#[cfg(feature = "serde")]
-impl serde::Serialize for LineEnding {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        match self {
-            LineEnding::Lf => serializer.serialize_str("lf"),
-            LineEnding::Crlf => serializer.serialize_str("crlf"),
-        }
-    }
-}
-
-#[cfg(feature = "serde")]
-impl<'de> serde::Deserialize<'de> for LineEnding {
-    fn deserialize<D>(deserializer: D) -> Result<LineEnding, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        match s.as_str() {
-            "lf" => Ok(LineEnding::Lf),
-            "crlf" => Ok(LineEnding::Crlf),
-            _ => Err(serde::de::Error::custom("invalid line ending")),
-        }
-    }
 }
