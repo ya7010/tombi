db96e55dcb82797a3ab3bce05b8907e0dbe47f18
yassun7010 <yassun7010@outlook.com>
2024-11-30 11:22:01 +0900

    refactor: module.

--- xtask/src/utils.rs
+++ xtask/src/utils.rs
@@ -1,11 +1,11 @@
 pub use crate::glue::pushenv;
 use crate::run;
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
 /// Returns the path to the root directory of `tombi` project.
 pub fn project_root() -> PathBuf {
-    let dir =
-        env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned());
+    let dir = std::env::var("CARGO_MANIFEST_DIR")
+        .unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned());
     PathBuf::from(dir).parent().unwrap().to_owned()
 }
 
