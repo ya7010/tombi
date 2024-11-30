72c096b83c76c9acc3393cde579b943099f6f37b
yassun7010 <yassun7010@outlook.com>
2024-11-30 11:26:19 +0900

    test: add test cases.

---
+++ crates/document/tests/test_toml_file.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+
+use config::TomlVersion;
+use document::Document;
+
+/// Returns the path to the root directory of `tombi` project.
+pub fn project_root() -> PathBuf {
+    let dir = std::env::var("CARGO_MANIFEST_DIR")
+        .unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned());
+    PathBuf::from(dir)
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_owned()
+}
+
+#[cfg(feature = "load")]
+#[test]
+fn load_tombi_toml() {
+    let toml_path = project_root().join("tombi.toml");
+    dbg!(&toml_path);
+    assert!(toml_path.exists());
+
+    let toml_source = std::fs::read_to_string(&toml_path).unwrap();
+    let document = Document::load(&toml_source, TomlVersion::default()).unwrap();
+
+    dbg!(document);
+}
