7d639b580ebd0a5381ad5f2d25d197d425b3d448
yassun7010 <yassun7010@outlook.com>
2024-12-09 00:32:18 +0900

    feat: add methods.

--- crates/ast/src/impls/array_of_tables.rs
+++ crates/ast/src/impls/array_of_tables.rs
@@ -1,4 +1,4 @@
-use crate::{support, AstNode};
+use crate::{support, AstChildren, AstNode, TableOrArrayOfTable};
 use syntax::T;
 
 impl crate::ArrayOfTables {
@@ -8,5 +8,26 @@ impl crate::ArrayOfTables {
 
     pub fn header_tailing_comment(&self) -> Option<crate::Comment> {
         support::tailing_comment(self.syntax().children_with_tokens(), T!("]]"))
+    }
+
+    fn parent_tables<'a>(&'a self) -> impl Iterator<Item = TableOrArrayOfTable> + 'a {
+        support::prev_siblings_nodes(self).take_while(|t: &TableOrArrayOfTable| {
+            self.header()
+                .unwrap()
+                .keys()
+                .starts_with(&t.header().unwrap().keys())
+        })
+    }
+
+    pub fn array_of_tables_keys<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = AstChildren<crate::Key>> + 'a {
+        self.parent_tables()
+            .filter_map(|parent_table| match parent_table {
+                crate::TableOrArrayOfTable::ArrayOfTables(array_of_tables) => {
+                    Some(array_of_tables.header().unwrap().keys())
+                }
+                _ => None,
+            })
     }
 }
