96f1e0c6afe7008d8dfd8e54e943798d2eaab3af
yassun7010 <yassun7010@outlook.com>
2024-12-16 01:45:16 +0900

    chore: debug code.

--- crates/parser/src/error.rs
+++ crates/parser/src/error.rs
@@ -96,7 +96,13 @@ impl Error {
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.kind.fmt(f)
+        write!(
+            f,
+            "{} in {}..{}",
+            self.kind,
+            self.range.start(),
+            self.range.end()
+        )
     }
 }
 
