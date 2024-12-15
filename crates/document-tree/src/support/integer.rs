133d3b9f00a4bec1ce4de57d293ff5e02f1ba366
yassun7010 <yassun7010@outlook.com>
2024-12-15 19:51:14 +0900

    refactor: split modules.

---
+++ crates/document-tree/src/support/integer.rs
@@ -0,0 +1,17 @@
+use std::num::ParseIntError;
+
+pub fn try_from_binary(value: &str) -> Result<i64, ParseIntError> {
+    i64::from_str_radix(&value[2..].replace('_', ""), 2)
+}
+
+pub fn try_from_octal(value: &str) -> Result<i64, ParseIntError> {
+    i64::from_str_radix(&value[2..].replace('_', ""), 8)
+}
+
+pub fn try_from_decimal(value: &str) -> Result<i64, ParseIntError> {
+    i64::from_str_radix(&value.replace('_', ""), 10)
+}
+
+pub fn try_from_hexadecimal(value: &str) -> Result<i64, ParseIntError> {
+    i64::from_str_radix(&value[2..].replace('_', ""), 16)
+}
