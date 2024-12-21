612328f215c9bf63b0c6afa3bbc6b7a85e4fc6a5
yassun7010 <yassun7010@outlook.com>
2024-12-22 00:25:03 +0900

    refactor: move trait impl order.

--- crates/formatter/src/format/value.rs
+++ crates/formatter/src/format/value.rs
@@ -11,6 +11,29 @@ use crate::Format;
 use std::fmt::Write;
 use syntax::SyntaxToken;
 
+impl Format for ast::Value {
+    fn fmt(&self, f: &mut crate::Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            Self::Array(it) => it.fmt(f),
+            Self::BasicString(it) => it.fmt(f),
+            Self::Boolean(it) => it.fmt(f),
+            Self::Float(it) => it.fmt(f),
+            Self::InlineTable(it) => it.fmt(f),
+            Self::IntegerBin(it) => it.fmt(f),
+            Self::IntegerDec(it) => it.fmt(f),
+            Self::IntegerHex(it) => it.fmt(f),
+            Self::IntegerOct(it) => it.fmt(f),
+            Self::LiteralString(it) => it.fmt(f),
+            Self::LocalDate(it) => it.fmt(f),
+            Self::LocalDateTime(it) => it.fmt(f),
+            Self::LocalTime(it) => it.fmt(f),
+            Self::MultiLineBasicString(it) => it.fmt(f),
+            Self::MultiLineLiteralString(it) => it.fmt(f),
+            Self::OffsetDateTime(it) => it.fmt(f),
+        }
+    }
+}
+
 trait LiteralNode {
     fn token(&self) -> Option<SyntaxToken>;
 }
@@ -32,28 +55,5 @@ where
         }
 
         Ok(())
-    }
-}
-
-impl Format for ast::Value {
-    fn fmt(&self, f: &mut crate::Formatter) -> Result<(), std::fmt::Error> {
-        match self {
-            Self::Array(it) => it.fmt(f),
-            Self::BasicString(it) => it.fmt(f),
-            Self::Boolean(it) => it.fmt(f),
-            Self::Float(it) => it.fmt(f),
-            Self::InlineTable(it) => it.fmt(f),
-            Self::IntegerBin(it) => it.fmt(f),
-            Self::IntegerDec(it) => it.fmt(f),
-            Self::IntegerHex(it) => it.fmt(f),
-            Self::IntegerOct(it) => it.fmt(f),
-            Self::LiteralString(it) => it.fmt(f),
-            Self::LocalDate(it) => it.fmt(f),
-            Self::LocalDateTime(it) => it.fmt(f),
-            Self::LocalTime(it) => it.fmt(f),
-            Self::MultiLineBasicString(it) => it.fmt(f),
-            Self::MultiLineLiteralString(it) => it.fmt(f),
-            Self::OffsetDateTime(it) => it.fmt(f),
-        }
     }
 }
