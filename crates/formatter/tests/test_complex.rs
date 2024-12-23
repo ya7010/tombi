955624debf890e4de3ee95735044319f12710f92
yassun7010 <yassun7010@outlook.com>
2024-12-24 08:51:23 +0900

    fix: features.

--- crates/formatter/tests/test_complex.rs
+++ crates/formatter/tests/test_complex.rs
@@ -1,4 +1,5 @@
 use assert_matches::assert_matches;
+use config::{FormatOptions, TomlVersion};
 
 #[test]
 fn test_key_values() {
@@ -11,7 +12,8 @@ bare-key = "value"
     .trim_start()
     .to_string();
 
-    let result = formatter::format(&source);
+    let result = formatter::Formatter::new(TomlVersion::default(), &FormatOptions::default())
+        .format(&source);
 
     assert_matches!(result, Ok(_));
     assert_eq!(result.unwrap(), source);
