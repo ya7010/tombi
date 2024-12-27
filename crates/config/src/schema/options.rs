eb168df8f694be6b3bb737a20fffe5df4d8a7723
yassun7010 <yassun7010@outlook.com>
2024-12-28 00:16:38 +0900

    feat: jsonschema.

--- crates/config/src/schema/options.rs
+++ crates/config/src/schema/options.rs
@@ -1,9 +1,14 @@
+use toml_version::TomlVersion;
+
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(deny_unknown_fields))]
 #[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
 #[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
 #[derive(Debug, Clone)]
 pub struct SchemaOptions {
+    /// # The TOML version that the schema is available.
+    toml_version: Option<TomlVersion>,
+
     /// # The schema path.
     path: String,
 
@@ -12,4 +17,20 @@ pub struct SchemaOptions {
     /// Supports glob pattern.
     #[cfg_attr(feature = "jsonschema", schemars(length(min = 1)))]
     include: Option<Vec<String>>,
+    // /// # The schema options for specific keys.
+    // #[cfg_attr(feature = "jsonschema", schemars(default))]
+    // subschemas: Option<Vec<SubSchemaOptions>>,
 }
+
+// #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+// #[cfg_attr(feature = "serde", serde(deny_unknown_fields))]
+// #[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+// #[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
+// #[derive(Debug, Clone)]
+// pub struct SubSchemaOptions {
+//     /// # The schema path.
+//     path: String,
+
+//     /// The path of the key to apply the schema.
+//     keys: String,
+// }
