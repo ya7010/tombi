a1a291b6120b56ec01e1f6b4463239d09c45ae93
yassun7010 <yassun7010@outlook.com>
2024-12-28 03:15:20 +0900

    feat: use draft07.

--- xtask/src/command/codegen_jsonschema.rs
+++ xtask/src/command/codegen_jsonschema.rs
@@ -1,8 +1,12 @@
+use schemars::{generate::SchemaSettings, SchemaGenerator};
+
 use crate::utils::project_root;
 
 pub fn run() -> Result<(), anyhow::Error> {
-    use schemars::schema_for;
-    let schema = schema_for!(config::Config);
+    let settings = SchemaSettings::draft07();
+    let generator = SchemaGenerator::new(settings);
+    let schema = generator.into_root_schema_for::<config::Config>();
+
     std::fs::write(
         project_root().join("tombi.schema.json"),
         serde_json::to_string_pretty(&schema)? + "\n",
