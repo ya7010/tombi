6060f4ca4cd56b851bd3ceced070a1061640f62b
yassun7010 <yassun7010@outlook.com>
2025-01-02 11:04:57 +0900

    refactor: use macro.

--- crates/toml-version/src/lib.rs
+++ crates/toml-version/src/lib.rs
@@ -1,26 +1,8 @@
-/// # TOML version.
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
-#[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[allow(non_camel_case_types)]
-pub enum TomlVersion {
-    #[default]
-    #[cfg_attr(feature = "serde", serde(rename = "v1.0.0"))]
-    #[cfg_attr(feature = "clap", value(name = "v1.0.0"))]
-    V1_0_0,
-
-    #[cfg_attr(feature = "serde", serde(rename = "v1.1.0-preview"))]
-    #[cfg_attr(feature = "clap", value(name = "v1.1.0-preview"))]
-    V1_1_0_Preview,
-}
-
-impl std::fmt::Display for TomlVersion {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::V1_0_0 => write!(f, "v1.0.0"),
-            Self::V1_1_0_Preview => write!(f, "v1.1.0-preview"),
-        }
+define_toml_version! {
+    pub enum TomlVersion {
+        #[default]
+        V1_0_0 => "v1.0.0",
+        V1_1_0_Preview => "v1.1.0-preview"
     }
 }
 
@@ -28,6 +10,38 @@ impl TomlVersion {
     pub const fn latest() -> Self {
         Self::V1_1_0_Preview
     }
+}
+
+#[macro_export]
+macro_rules! define_toml_version {
+    (
+        pub enum TomlVersion {
+            $($(#[$attr:meta])* $variant:ident => $version:literal),* $(,)?
+        }
+    ) => {
+        /// # TOML version.
+        #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+        #[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
+        #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
+        #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        #[allow(non_camel_case_types)]
+        pub enum TomlVersion {
+            $(
+                $(#[$attr])*
+                #[cfg_attr(feature = "serde", serde(rename = $version))]
+                #[cfg_attr(feature = "clap", value(name = $version))]
+                $variant,
+            )*
+        }
+
+        impl std::fmt::Display for TomlVersion {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    $(Self::$variant => write!(f, $version),)*
+                }
+            }
+        }
+    };
 }
 
 #[cfg(test)]
