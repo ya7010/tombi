cd0e826c7063ffe8c5dbc7a88c549b8e96fa5984
yassun7010 <yassun7010@outlook.com>
2025-01-16 09:50:37 +0900

    wip: schema.

--- crates/server/src/hover/table.rs
+++ crates/server/src/hover/table.rs
@@ -17,24 +17,6 @@ impl GetHoverContent for document_tree::Table {
             if let Some(value) = self.get(key) {
                 let accessor = Accessor::Key(key.to_raw_text(toml_version));
 
-                let value_schema = match value_schema {
-                    Some(ValueSchema::Table(table)) => match table.properties.get_mut(&accessor) {
-                        Some(mut schema) => {
-                            let schema_value = schema.value_mut();
-                            schema_value.resolve(definitions).ok();
-                            return value.get_hover_content(
-                                accessors,
-                                value_schema,
-                                toml_version,
-                                position,
-                                &keys[1..],
-                                definitions,
-                            );
-                        }
-                        None => None,
-                    },
-                    _ => value_schema,
-                };
                 accessors.push(accessor);
 
                 return value.get_hover_content(
