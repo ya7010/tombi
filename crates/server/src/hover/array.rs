5274b52f92c8452ada949179ce590ad13e641dd7
yassun7010 <yassun7010@outlook.com>
2025-01-17 01:03:04 +0900

    feat: array type.

--- crates/server/src/hover/array.rs
+++ crates/server/src/hover/array.rs
@@ -1,4 +1,7 @@
-use super::GetHoverContent;
+use super::{
+    value::{get_all_of_hover_content, get_any_of_hover_content, get_one_of_hover_content},
+    GetHoverContent, HoverContent,
+};
 
 impl GetHoverContent for document_tree::Array {
     fn get_hover_content(
@@ -14,6 +17,67 @@ impl GetHoverContent for document_tree::Array {
             if value.range().contains(position) {
                 let accessor = schema_store::Accessor::Index(index);
 
+                match value_schema {
+                    Some(schema_store::ValueSchema::Array(array)) => {
+                        if let Some(items) = &array.items {
+                            if let Ok(mut item) = items.write() {
+                                return value.get_hover_content(
+                                    &accessors
+                                        .clone()
+                                        .into_iter()
+                                        .chain(std::iter::once(accessor))
+                                        .collect(),
+                                    item.resolve(definitions).ok(),
+                                    toml_version,
+                                    position,
+                                    keys,
+                                    definitions,
+                                );
+                            }
+                        }
+                    }
+                    Some(schema_store::ValueSchema::OneOf(one_of_schema)) => {
+                        if let Some(hover_content) = get_one_of_hover_content(
+                            self,
+                            accessors,
+                            one_of_schema,
+                            toml_version,
+                            position,
+                            keys,
+                            definitions,
+                        ) {
+                            return Some(hover_content);
+                        }
+                    }
+                    Some(schema_store::ValueSchema::AnyOf(any_of_schema)) => {
+                        if let Some(hover_content) = get_any_of_hover_content(
+                            self,
+                            accessors,
+                            any_of_schema,
+                            toml_version,
+                            position,
+                            keys,
+                            definitions,
+                        ) {
+                            return Some(hover_content);
+                        }
+                    }
+                    Some(schema_store::ValueSchema::AllOf(all_of_schema)) => {
+                        if let Some(hover_content) = get_all_of_hover_content(
+                            self,
+                            accessors,
+                            all_of_schema,
+                            toml_version,
+                            position,
+                            keys,
+                            definitions,
+                        ) {
+                            return Some(hover_content);
+                        }
+                    }
+                    Some(_) => return None,
+                    None => {}
+                }
                 return value.get_hover_content(
                     &accessors
                         .clone()
@@ -29,23 +93,68 @@ impl GetHoverContent for document_tree::Array {
             }
         }
 
-        let keys = schema_store::Accessors::new(accessors.clone());
-        let value_type = schema_store::ValueType::Array;
+        match value_schema {
+            Some(schema_store::ValueSchema::Array(array)) => {
+                let mut hover_content = HoverContent {
+                    keys: schema_store::Accessors::new(accessors.clone()),
+                    value_type: schema_store::ValueType::Array,
+                    ..Default::default()
+                };
 
-        if let Some(schema_store::ValueSchema::Array(schema)) = value_schema {
-            Some(super::HoverContent {
-                title: schema.title.clone(),
-                description: schema.description.clone(),
-                keys,
-                value_type,
-                ..Default::default()
-            })
-        } else {
-            Some(super::HoverContent {
-                keys,
-                value_type,
-                ..Default::default()
-            })
+                if let Some(title) = &array.title {
+                    hover_content.title = Some(title.clone());
+                }
+                if let Some(description) = &array.description {
+                    hover_content.description = Some(description.clone());
+                }
+                return Some(hover_content);
+            }
+            Some(schema_store::ValueSchema::OneOf(one_of_schema)) => {
+                if let Some(hover_content) = get_one_of_hover_content(
+                    self,
+                    accessors,
+                    one_of_schema,
+                    toml_version,
+                    position,
+                    keys,
+                    definitions,
+                ) {
+                    return Some(hover_content);
+                }
+            }
+            Some(schema_store::ValueSchema::AnyOf(any_of_schema)) => {
+                if let Some(hover_content) = get_any_of_hover_content(
+                    self,
+                    accessors,
+                    any_of_schema,
+                    toml_version,
+                    position,
+                    keys,
+                    definitions,
+                ) {
+                    return Some(hover_content);
+                }
+            }
+            Some(schema_store::ValueSchema::AllOf(all_of_schema)) => {
+                if let Some(hover_content) = get_all_of_hover_content(
+                    self,
+                    accessors,
+                    all_of_schema,
+                    toml_version,
+                    position,
+                    keys,
+                    definitions,
+                ) {
+                    return Some(hover_content);
+                }
+            }
+            Some(_) => return None,
+            None => {}
         }
+        Some(super::HoverContent {
+            keys: schema_store::Accessors::new(accessors.clone()),
+            value_type: schema_store::ValueType::Array,
+            ..Default::default()
+        })
     }
 }
