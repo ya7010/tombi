a03fcc2ce5324273b708b4fd4356854e36d6e9fd
yassun7010 <yassun7010@outlook.com>
2025-01-25 23:09:13 +0900

    wip: add table candidates.

--- crates/server/src/completion/table.rs
+++ crates/server/src/completion/table.rs
@@ -68,20 +68,76 @@ impl FindCompletionItems for TableSchema {
 impl FindCompletionItems2 for document_tree::Table {
     fn find_completion_items2(
         &self,
-        _accessors: &Vec<Accessor>,
-        _value_schema: &ValueSchema,
+        accessors: &Vec<Accessor>,
+        value_schema: &ValueSchema,
         _toml_version: TomlVersion,
         _position: text::Position,
         _keys: &[document_tree::Key],
         _schema_url: Option<&Url>,
-        _definitions: &SchemaDefinitions,
-        _completion_hint: Option<CompletionHint>,
+        definitions: &SchemaDefinitions,
+        completion_hint: Option<CompletionHint>,
     ) -> (
         Vec<tower_lsp::lsp_types::CompletionItem>,
         Vec<schema_store::Error>,
     ) {
-        let completions = Vec::new();
-        let errors = Vec::new();
+        let mut completions = Vec::new();
+        let mut errors = Vec::new();
+
+        if accessors.is_empty() {
+            match value_schema {
+                ValueSchema::Table(table_schema) => {
+                    for mut property in table_schema.properties.iter_mut() {
+                        let key = property.key().to_string();
+                        if let Ok(value_schema) = property.value_mut().resolve(definitions) {
+                            let (schema_candidates, schema_errors) =
+                                value_schema.find_schema_candidates(accessors, definitions);
+                            for schema_candidate in schema_candidates {
+                                match completion_hint {
+                                    Some(CompletionHint::InTableHeader) => {
+                                        if !value_schema.is_match(&|s| {
+                                            matches!(
+                                                s,
+                                                ValueSchema::Table(_) | ValueSchema::Array(_)
+                                            )
+                                        }) {
+                                            continue;
+                                        }
+                                    }
+                                    _ => {}
+                                }
+
+                                let completion_item = tower_lsp::lsp_types::CompletionItem {
+                                    label: key.to_string(),
+                                    kind: Some(CompletionItemKind::PROPERTY),
+                                    detail: schema_candidate.detail(definitions, completion_hint),
+                                    documentation: schema_candidate
+                                        .documentation(definitions, completion_hint),
+                                    ..Default::default()
+                                };
+                                completions.push(completion_item);
+                            }
+                            errors.extend(schema_errors);
+                        }
+                    }
+                }
+                _ => {}
+            }
+        } else {
+            match value_schema {
+                ValueSchema::Table(table_schema) => {
+                    if let Some(mut property) = table_schema.properties.get_mut(&accessors[0]) {
+                        if let Ok(schema) = property.value_mut().resolve(&definitions) {
+                            return schema.find_completion_items(
+                                &accessors[1..],
+                                &definitions,
+                                completion_hint,
+                            );
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
 
         (completions, errors)
     }
