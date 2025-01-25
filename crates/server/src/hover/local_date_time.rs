f54a6699b08a76156b981329f1863b294e9fe48a
yassun7010 <yassun7010@outlook.com>
2025-01-26 01:51:08 +0900

    refactor: local_date_time hover.

--- crates/server/src/hover/local_date_time.rs
+++ crates/server/src/hover/local_date_time.rs
@@ -15,78 +15,60 @@ impl GetHoverContent for document_tree::LocalDateTime {
         schema_url: Option<&Url>,
         definitions: &schema_store::SchemaDefinitions,
     ) -> Option<super::HoverContent> {
-        let value_type = schema_store::ValueType::LocalDateTime;
-
         match value_schema {
-            Some(schema_store::ValueSchema::LocalDateTime(schema)) => {
-                return Some(super::HoverContent {
-                    title: schema.title.clone(),
-                    description: schema.description.clone(),
-                    accessors: schema_store::Accessors::new(accessors.clone()),
-                    value_type,
-                    enumerated_values: schema
-                        .enumerate
-                        .as_ref()
-                        .map(|v| v.iter().map(ToString::to_string).collect())
-                        .unwrap_or_default(),
-                    schema_url: schema_url.cloned(),
-                    range: Some(self.range()),
-                })
-            }
-            Some(schema_store::ValueSchema::OneOf(one_of_schema)) => {
-                if let Some(hover_content) = get_one_of_hover_content(
-                    self,
-                    accessors,
-                    one_of_schema,
-                    toml_version,
-                    position,
-                    keys,
-                    schema_url,
-                    definitions,
-                ) {
-                    return Some(hover_content);
-                }
-            }
-            Some(schema_store::ValueSchema::AnyOf(any_of_schema)) => {
-                if let Some(hover_content) = get_any_of_hover_content(
-                    self,
-                    accessors,
-                    any_of_schema,
-                    toml_version,
-                    position,
-                    keys,
-                    schema_url,
-                    definitions,
-                ) {
-                    return Some(hover_content);
-                }
-            }
-            Some(schema_store::ValueSchema::AllOf(all_of_schema)) => {
-                if let Some(hover_content) = get_all_of_hover_content(
-                    self,
-                    accessors,
-                    all_of_schema,
-                    toml_version,
-                    position,
-                    keys,
-                    schema_url,
-                    definitions,
-                ) {
-                    return Some(hover_content);
-                }
-            }
-            Some(_) => return None,
-            None => {}
+            Some(schema_store::ValueSchema::LocalDateTime(schema)) => Some(super::HoverContent {
+                title: schema.title.clone(),
+                description: schema.description.clone(),
+                accessors: schema_store::Accessors::new(accessors.clone()),
+                value_type: schema_store::ValueType::LocalDateTime,
+                enumerated_values: schema
+                    .enumerate
+                    .as_ref()
+                    .map(|v| v.iter().map(ToString::to_string).collect())
+                    .unwrap_or_default(),
+                schema_url: schema_url.cloned(),
+                range: Some(self.range()),
+            }),
+            Some(schema_store::ValueSchema::OneOf(one_of_schema)) => get_one_of_hover_content(
+                self,
+                accessors,
+                one_of_schema,
+                toml_version,
+                position,
+                keys,
+                schema_url,
+                definitions,
+            ),
+            Some(schema_store::ValueSchema::AnyOf(any_of_schema)) => get_any_of_hover_content(
+                self,
+                accessors,
+                any_of_schema,
+                toml_version,
+                position,
+                keys,
+                schema_url,
+                definitions,
+            ),
+            Some(schema_store::ValueSchema::AllOf(all_of_schema)) => get_all_of_hover_content(
+                self,
+                accessors,
+                all_of_schema,
+                toml_version,
+                position,
+                keys,
+                schema_url,
+                definitions,
+            ),
+            Some(_) => None,
+            None => Some(super::HoverContent {
+                title: None,
+                description: None,
+                accessors: schema_store::Accessors::new(accessors.clone()),
+                value_type: schema_store::ValueType::LocalDateTime,
+                enumerated_values: vec![],
+                schema_url: None,
+                range: Some(self.range()),
+            }),
         }
-
-        Some(super::HoverContent {
-            title: None,
-            description: None,
-            accessors: schema_store::Accessors::new(accessors.clone()),
-            value_type,
-            enumerated_values: vec![],
-            schema_url: None,
-            range: Some(self.range()),
-        })
     }
 }
