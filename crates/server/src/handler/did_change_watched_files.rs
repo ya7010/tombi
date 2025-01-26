86d4f7d1a71d0bd213a93435d8070bb1f9e6869a
yassun7010 <yassun7010@outlook.com>
2025-01-26 20:25:12 +0900

    chore: add tracing.

--- crates/server/src/handler/did_change_watched_files.rs
+++ crates/server/src/handler/did_change_watched_files.rs
@@ -3,4 +3,5 @@ use tower_lsp::lsp_types::DidChangeWatchedFilesParams;
 pub async fn handle_did_change_watched_files(
     DidChangeWatchedFilesParams { .. }: DidChangeWatchedFilesParams,
 ) {
+    tracing::info!("handle_did_change_watched_files");
 }
