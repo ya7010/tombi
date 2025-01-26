0166a7ec27bf0b0dec3b71b9dda897c95842a29d
yassun7010 <yassun7010@outlook.com>
2025-01-26 19:53:29 +0900

    test: add test cases.

--- crates/server/src/handler/completion.rs
+++ crates/server/src/handler/completion.rs
@@ -165,3 +165,209 @@ fn get_completion_items(
 
     completion_items
 }
+
+#[cfg(test)]
+mod test {
+    use itertools::Itertools;
+
+    use crate::{
+        handler::handle_did_open,
+        test::{cargo_schema_path, pyproject_schema_path, tombi_schema_path},
+    };
+
+    use super::*;
+
+    #[macro_export]
+    macro_rules! test_completion_labels {
+        (
+            #[tokio::test]
+            async fn $name:ident(
+                $schema_file_path:expr,
+                $source:expr
+            ) -> Ok([$($label:expr),*$(,)?]);
+        ) => {
+            #[tokio::test]
+            async fn $name() {
+                use backend::Backend;
+                use schema_store::JsonCatalogSchema;
+                use std::io::Write;
+                use tower_lsp::{
+                    lsp_types::{
+                        DidOpenTextDocumentParams, PartialResultParams, TextDocumentIdentifier,
+                        TextDocumentItem, Url, WorkDoneProgressParams,
+                    },
+                    LspService,
+                };
+
+                let (service, _) = LspService::new(|client| Backend::new(client));
+
+                let backend = service.inner();
+
+                let schema_url = Url::from_file_path($schema_file_path).expect(
+                    format!(
+                        "failed to convert schema path to URL: {}",
+                        tombi_schema_path().display()
+                    )
+                    .as_str(),
+                );
+                backend
+                    .schema_store
+                    .add_catalog(JsonCatalogSchema {
+                        name: "test_schema".to_string(),
+                        description: "schema for testing".to_string(),
+                        file_match: vec!["*.toml".to_string()],
+                        url: schema_url.clone(),
+                    })
+                    .await;
+
+                let temp_file = tempfile::NamedTempFile::with_suffix_in(
+                    ".toml",
+                    std::env::current_dir().expect("failed to get current directory"),
+                )
+                .expect("failed to create temporary file");
+
+                let mut toml_data = textwrap::dedent($source).trim().to_string();
+
+                let index = toml_data
+                    .as_str()
+                    .find("█")
+                    .expect("failed to find completion position marker (█) in the test data");
+
+                toml_data.remove(index);
+                temp_file.as_file().write_all(toml_data.as_bytes()).expect(
+                    "failed to write test data to the temporary file, which is used as a text document",
+                );
+
+                let toml_file_url = Url::from_file_path(temp_file.path())
+                    .expect("failed to convert temporary file path to URL");
+
+                handle_did_open(
+                    backend,
+                    DidOpenTextDocumentParams {
+                        text_document: TextDocumentItem {
+                            uri: toml_file_url.clone(),
+                            language_id: "toml".to_string(),
+                            version: 0,
+                            text: toml_data.clone(),
+                        },
+                    },
+                )
+                .await;
+
+                let completions = handle_completion(
+                    &backend,
+                    CompletionParams {
+                        text_document_position: TextDocumentPositionParams {
+                            text_document: TextDocumentIdentifier { uri: toml_file_url },
+                            position: (text::Position::default()
+                                + text::RelativePosition::of(&toml_data[..index]))
+                            .into(),
+                        },
+                        work_done_progress_params: WorkDoneProgressParams::default(),
+                        partial_result_params: PartialResultParams {
+                            partial_result_token: None,
+                        },
+                        context: None,
+                    },
+                )
+                .await
+                .expect("failed to handle completion")
+                .expect("failed to get completion items");
+
+                match completions {
+                    CompletionResponse::Array(items) => {
+                        let labels = items
+                            .into_iter()
+                            .sorted_by(|a, b|
+                                a.sort_text.as_ref().unwrap_or(&a.label).cmp(&b.sort_text.as_ref().unwrap_or(&b.label))
+                            )
+                            .map(|item| item.label)
+                            .collect::<Vec<_>>();
+
+                        pretty_assertions::assert_eq!(
+                            labels,
+                            vec![$($label.to_string()),*]
+                        );
+                    }
+                    _ => panic!("expected completion items"),
+                }
+            }
+        };
+    }
+
+    test_completion_labels! {
+        #[tokio::test]
+        async fn tombi_empty(
+            tombi_schema_path(),
+            "█"
+        ) -> Ok([
+            "format",
+            "lint",
+            "schema",
+            "schemas",
+            "server",
+            "toml-version",
+        ]);
+    }
+
+    test_completion_labels! {
+        #[tokio::test]
+        async fn tombi_used_toml_version(
+            tombi_schema_path(),
+            r#"
+            toml-version = "v1.0.0"
+            █
+            "#
+        ) -> Ok([
+            "format",
+            "lint",
+            "schema",
+            "schemas",
+            "server",
+            // "toml-version",
+        ]);
+    }
+
+    test_completion_labels! {
+        #[tokio::test]
+        async fn pyproject_empty(
+            pyproject_schema_path(),
+            "█"
+        ) -> Ok([
+            "build-system",
+            "dependency-groups",
+            "project",
+            "tool",
+        ]);
+    }
+
+    test_completion_labels! {
+        #[tokio::test]
+        async fn cargo_empty(
+            cargo_schema_path(),
+            "█"
+        ) -> Ok([
+            "badges",
+            "bench",
+            "bin",
+            "build-dependencies",
+            "build_dependencies",
+            "cargo-features",
+            "dependencies",
+            "dev-dependencies",
+            "dev_dependencies",
+            "example",
+            "features",
+            "lib",
+            "lints",
+            "package",
+            "patch",
+            "profile",
+            "project",
+            "replace",
+            "target",
+            "test",
+            "workspace",
+        ]);
+    }
+}
