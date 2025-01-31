fee68cbcec4af4a958c0e7c22d33e1c2ba2e58b1
yassun7010 <yassun7010@outlook.com>
2025-01-31 20:32:22 +0900

    refactor: unuse completions.

--- crates/server/src/completion/value/float.rs
+++ crates/server/src/completion/value/float.rs
@@ -3,70 +3,8 @@ use schema_store::{Accessor, FloatSchema, SchemaDefinitions, ValueSchema};
 use tower_lsp::lsp_types::Url;
 
 use crate::completion::{
-    find_all_if_completion_items, find_any_of_completion_items, find_one_of_completion_items,
     CompletionContent, CompletionEdit, CompletionHint, FindCompletionContents,
 };
-
-impl FindCompletionContents for document_tree::Float {
-    fn find_completion_contents(
-        &self,
-        accessors: &Vec<Accessor>,
-        value_schema: &ValueSchema,
-        toml_version: TomlVersion,
-        position: text::Position,
-        keys: &[document_tree::Key],
-        schema_url: Option<&Url>,
-        definitions: &SchemaDefinitions,
-        completion_hint: Option<CompletionHint>,
-    ) -> Vec<CompletionContent> {
-        match value_schema {
-            ValueSchema::Float(float_schema) => float_schema.find_completion_contents(
-                accessors,
-                value_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            ValueSchema::OneOf(one_of_schema) => find_one_of_completion_items(
-                self,
-                accessors,
-                one_of_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            ValueSchema::AnyOf(any_of_schema) => find_any_of_completion_items(
-                self,
-                accessors,
-                any_of_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            ValueSchema::AllOf(all_of_schema) => find_all_if_completion_items(
-                self,
-                accessors,
-                all_of_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            _ => Vec::with_capacity(0),
-        }
-    }
-}
 
 impl FindCompletionContents for FloatSchema {
     fn find_completion_contents(
--- crates/server/src/completion/value/string.rs
+++ crates/server/src/completion/value/string.rs
@@ -3,70 +3,8 @@ use schema_store::{Accessor, SchemaDefinitions, StringSchema, ValueSchema};
 use tower_lsp::lsp_types::Url;
 
 use crate::completion::{
-    find_all_if_completion_items, find_any_of_completion_items, find_one_of_completion_items,
     CompletionContent, CompletionEdit, CompletionHint, FindCompletionContents,
 };
-
-impl FindCompletionContents for document_tree::String {
-    fn find_completion_contents(
-        &self,
-        accessors: &Vec<Accessor>,
-        value_schema: &ValueSchema,
-        toml_version: TomlVersion,
-        position: text::Position,
-        keys: &[document_tree::Key],
-        schema_url: Option<&Url>,
-        definitions: &SchemaDefinitions,
-        completion_hint: Option<CompletionHint>,
-    ) -> Vec<CompletionContent> {
-        match value_schema {
-            ValueSchema::String(string_schema) => string_schema.find_completion_contents(
-                accessors,
-                value_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            ValueSchema::OneOf(one_of_schema) => find_one_of_completion_items(
-                self,
-                accessors,
-                one_of_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            ValueSchema::AnyOf(any_of_schema) => find_any_of_completion_items(
-                self,
-                accessors,
-                any_of_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            ValueSchema::AllOf(all_of_schema) => find_all_if_completion_items(
-                self,
-                accessors,
-                all_of_schema,
-                toml_version,
-                position,
-                keys,
-                schema_url,
-                definitions,
-                completion_hint,
-            ),
-            _ => Vec::with_capacity(0),
-        }
-    }
-}
 
 impl FindCompletionContents for StringSchema {
     fn find_completion_contents(
