8ac0a454f490467526201e9e6942c1db8bb7da17
yassun7010 <yassun7010@outlook.com>
2025-02-02 13:31:48 +0900

    chore: add note.

--- crates/server/src/completion/completion_kind.rs
+++ crates/server/src/completion/completion_kind.rs
@@ -17,11 +17,14 @@ pub enum CompletionKind {
 
 impl From<CompletionKind> for tower_lsp::lsp_types::CompletionItemKind {
     fn from(kind: CompletionKind) -> Self {
+        // NOTE: All TOML completions are CompletionItemKind::VALUE,
+        //       but some are assigned different types to make it easier to distinguish by symbols.
         match kind {
             CompletionKind::Boolean => tower_lsp::lsp_types::CompletionItemKind::ENUM_MEMBER,
             CompletionKind::Integer => tower_lsp::lsp_types::CompletionItemKind::VALUE,
             CompletionKind::Float => tower_lsp::lsp_types::CompletionItemKind::VALUE,
             CompletionKind::String => tower_lsp::lsp_types::CompletionItemKind::TEXT,
+            // NOTE: Event is related to time
             CompletionKind::OffsetDateTime => tower_lsp::lsp_types::CompletionItemKind::EVENT,
             CompletionKind::LocalDateTime => tower_lsp::lsp_types::CompletionItemKind::EVENT,
             CompletionKind::LocalDate => tower_lsp::lsp_types::CompletionItemKind::EVENT,
@@ -29,6 +32,7 @@ impl From<CompletionKind> for tower_lsp::lsp_types::CompletionItemKind {
             CompletionKind::Array => tower_lsp::lsp_types::CompletionItemKind::VALUE,
             CompletionKind::Table => tower_lsp::lsp_types::CompletionItemKind::STRUCT,
             CompletionKind::Property => tower_lsp::lsp_types::CompletionItemKind::FIELD,
+            // NOTE: To give a writing taste close to method chaining
             CompletionKind::MagicTrigger => tower_lsp::lsp_types::CompletionItemKind::METHOD,
         }
     }
