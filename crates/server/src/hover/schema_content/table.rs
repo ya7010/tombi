7885d500828bd1603cd6d0e3d843317e437bb03c
yassun7010 <yassun7010@outlook.com>
2025-02-06 01:55:37 +0900

    feat: rename property to key.

--- crates/server/src/hover/schema_content/table.rs
+++ crates/server/src/hover/schema_content/table.rs
@@ -7,15 +7,15 @@ impl SchemaHoverContent for TableSchema {
         let mut content = String::new();
 
         if let Some(min_properties) = self.min_properties {
-            content.push_str(&format!("Minimum Properties: `{}`\n\n", min_properties));
+            content.push_str(&format!("Minimum Keys: `{}`\n\n", min_properties));
         }
 
         if let Some(max_properties) = self.max_properties {
-            content.push_str(&format!("Maximum Properties: `{}`\n\n", max_properties));
+            content.push_str(&format!("Maximum Keys: `{}`\n\n", max_properties));
         }
 
         if let Some(pattern_properties) = &self.pattern_properties {
-            content.push_str("Pattern Properties:\n\n");
+            content.push_str("Pattern Keys:\n\n");
             for pattern_property in pattern_properties.iter() {
                 content.push_str(&format!("- `{}`\n\n", pattern_property.key()));
             }
@@ -23,7 +23,7 @@ impl SchemaHoverContent for TableSchema {
 
         if self.additional_properties {
             content.push_str(&format!(
-                "Additional Properties: `{}`\n\n",
+                "Additional Keys: `{}`\n\n",
                 self.additional_properties
             ));
         }
