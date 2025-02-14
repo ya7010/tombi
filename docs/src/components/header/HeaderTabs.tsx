396f3b301f25872fe1405ccd0f6bbdb7ed8dd897
yassun7010 <yassun7010@outlook.com>
2025-02-15 00:10:40 +0900

    chore: tabs.

--- docs/src/components/header/HeaderTabs.tsx
+++ docs/src/components/header/HeaderTabs.tsx
@@ -3,7 +3,6 @@ import { HeaderTab } from "./HeaderTab";
 export function HeaderTabs() {
   return (
     <div class="hidden md:flex items-center space-x-8">
-      <HeaderTab href="/concepts">Concepts</HeaderTab>
       <HeaderTab href="/documentation">Docs</HeaderTab>
       <HeaderTab href="/playground">Playground</HeaderTab>
     </div>
