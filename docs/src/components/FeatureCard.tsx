6b6310cde0a5a1056cc6da23deb0cc0b13dfc1ac
yassun7010 <yassun7010@outlook.com>
2025-02-14 16:46:55 +0900

    chore: card background.

--- docs/src/components/FeatureCard.tsx
+++ docs/src/components/FeatureCard.tsx
@@ -6,7 +6,7 @@ interface FeatureCardProps {
 
 export function FeatureCard(props: FeatureCardProps) {
   return (
-    <div class="p-6 rounded-lg bg-gray-50 dark:bg-gray-800">
+    <div class="p-6 rounded-lg bg-gray-200 dark:bg-gray-800">
       <h3 class="text-xl font-semibold mb-2">
         {props.emoji} {props.title}
       </h3>
