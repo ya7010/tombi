08109de7dedc2d12a3ff3d1db72e32421eef3fcd
yassun7010 <yassun7010@outlook.com>
2025-02-15 19:26:05 +0900

    chore: color.

--- docs/src/components/search/SearchResults.tsx
+++ docs/src/components/search/SearchResults.tsx
@@ -70,7 +70,7 @@ function HighlightedText(props: { text: string; matches: [number, number][] }) {
 export function SearchResults(props: SearchResultsProps) {
   return (
     <Show when={props.isVisible && props.results.length > 0}>
-      <div class="fixed left-0 right-0 mt-2 mx-auto max-w-150 bg-white/95 dark:bg-gray-800/95 rounded-lg shadow-lg max-h-[80vh] overflow-y-auto z-[60]" style={{ top: "calc(5rem + 4px)" }}>
+      <div class="fixed left-0 right-0 mt-2 mx-auto max-w-150 bg-white dark:bg-gray-800 rounded-lg shadow-lg max-h-[80vh] overflow-y-auto z-[60]" style={{ top: "calc(5rem + 4px)" }}>
         <div class="p-4">
           <For each={props.results}>
             {(result) => (
