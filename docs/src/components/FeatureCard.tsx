4a988d07caf0d7651406eeec9ddd90ade588a1de
yassun7010 <yassun7010@outlook.com>
2025-02-15 16:44:05 +0900

    chore: text size.

--- docs/src/components/FeatureCard.tsx
+++ docs/src/components/FeatureCard.tsx
@@ -7,10 +7,10 @@ interface FeatureCardProps {
 export function FeatureCard(props: FeatureCardProps) {
   return (
     <div class="p-6 rounded-lg bg-gray-200 dark:bg-gray-800">
-      <h3 class="text-xl font-semibold mb-2">
+      <h3 class="text-2xl font-semibold mb-2">
         {props.emoji} {props.title}
       </h3>
-      <p class="text-lg text-gray-600 dark:text-gray-400">
+      <p class="text-gray-600 dark:text-gray-400">
         {props.description}
       </p>
     </div>
