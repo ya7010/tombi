385cc485e97c1f77d1dbb647c9808be9a43cdcb4
yassun7010 <yassun7010@outlook.com>
2025-02-15 17:10:27 +0900

    chore: logo size

--- docs/src/routes/index.tsx
+++ docs/src/routes/index.tsx
@@ -32,11 +32,11 @@ export default function Home() {
 
       <section class="text-center mb-24">
           <h1 class="sr-only">Tombi</h1>
-          <div class="relative mb-16 w-screen -mx-[calc((100vw-100%)/2)] overflow-hidden bg-gradient-to-b from-gray-900 to-gray-500">
+          <div class="relative py-8 w-screen -mx-[calc((100vw-100%)/2)] overflow-hidden bg-gradient-to-b from-gray-900 to-gray-500">
               <img
                 src="/tombi-transparent.svg"
                 alt="Tombi Logo"
-                class="w-auto max-h-80 mx-auto"
+                class="w-auto max-h-80 mx-auto px-8"
               />
           </div>
 
