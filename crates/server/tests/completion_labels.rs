58babf254965498b4fee4a8b6d2280d0879005ed
yassun7010 <yassun7010@outlook.com>
2025-02-26 23:50:21 +0900

    refactor: test code.

--- crates/server/tests/completion_labels.rs
+++ crates/server/tests/completion_labels.rs
@@ -710,227 +710,235 @@ mod completion_labels {
             ) -> Ok(AnyValue);
         }
     }
-}
 
-#[macro_export]
-macro_rules! test_completion_labels {
-    (
-        #[tokio::test]
-        async fn $name:ident(
-            $source:expr,
-            $schema_file_path:expr$(,)?
-        ) -> Ok([$($label:expr),*$(,)?]);
-    ) => {
-        test_completion_labels! {
+    #[macro_export]
+    macro_rules! test_completion_labels {
+        (
             #[tokio::test]
-            async fn _$name(
-                $source,
-                Some($schema_file_path),
-            ) -> Ok([$($label),*]);
-        }
-    };
+            async fn $name:ident(
+                $source:expr,
+                $schema_file_path:expr$(,)?
+            ) -> Ok([$($label:expr),*$(,)?]);
+        ) => {
+            test_completion_labels! {
+                #[tokio::test]
+                async fn _$name(
+                    $source,
+                    Some($schema_file_path),
+                ) -> Ok([$($label),*]);
+            }
+        };
 
-    (
-        #[tokio::test]
-        async fn $name:ident(
-            $source:expr$(,)?
-        ) -> Ok([$($label:expr),*$(,)?]);
-    ) => {
-        test_completion_labels! {
+        (
             #[tokio::test]
-            async fn _$name(
-                $source,
-                Option::<std::path::PathBuf>::None,
-            ) -> Ok([$($label),*]);
-        }
-    };
+            async fn $name:ident(
+                $source:expr$(,)?
+            ) -> Ok([$($label:expr),*$(,)?]);
+        ) => {
+            test_completion_labels! {
+                #[tokio::test]
+                async fn _$name(
+                    $source,
+                    Option::<std::path::PathBuf>::None,
+                ) -> Ok([$($label),*]);
+            }
+        };
 
-    (
-        #[tokio::test]
-        async fn $name:ident(
-            $source:expr,
-            $schema_file_path:expr$(,)?
-        ) -> Ok(AnyValue);
-    ) => {
-        test_completion_labels! {
+        (
             #[tokio::test]
-            async fn _$name(
-                $source,
-                Some($schema_file_path),
+            async fn $name:ident(
+                $source:expr,
+                $schema_file_path:expr$(,)?
             ) -> Ok(AnyValue);
-        }
-    };
+        ) => {
+            test_completion_labels! {
+                #[tokio::test]
+                async fn _$name(
+                    $source,
+                    Some($schema_file_path),
+                ) -> Ok(AnyValue);
+            }
+        };
 
-    (
-        #[tokio::test]
-        async fn $name:ident(
-            $source:expr$(,)?
-        ) -> Ok(AnyValue);
-    ) => {
-        test_completion_labels! {
+        (
             #[tokio::test]
-            async fn _$name(
-                $source,
-                Option::<std::path::PathBuf>::None,
+            async fn $name:ident(
+                $source:expr$(,)?
             ) -> Ok(AnyValue);
-        }
-    };
+        ) => {
+            test_completion_labels! {
+                #[tokio::test]
+                async fn _$name(
+                    $source,
+                    Option::<std::path::PathBuf>::None,
+                ) -> Ok(AnyValue);
+            }
+        };
 
-    (
-        #[tokio::test]
-        async fn _$name:ident(
-            $source:expr,
-            $schema_file_path:expr$(,)?
-        ) -> Ok(AnyValue);
-    ) => {
-        test_completion_labels! {
+        (
             #[tokio::test]
-            async fn _$name(
-                $source,
-                $schema_file_path,
-            ) -> Ok([
-                "\"\"",
-                "''",
-                today_local_time(),
-                today_local_date(),
-                today_local_date_time(),
-                today_offset_date_time(),
-                "3.14",
-                "42",
-                "[]",
-                "{}",
-                "$key",
-                "true",
-                "false",
-            ]);
-        }
-    };
+            async fn _$name:ident(
+                $source:expr,
+                $schema_file_path:expr$(,)?
+            ) -> Ok(AnyValue);
+        ) => {
+            test_completion_labels! {
+                #[tokio::test]
+                async fn _$name(
+                    $source,
+                    $schema_file_path,
+                ) -> Ok([
+                    "\"\"",
+                    "''",
+                    today_local_time(),
+                    today_local_date(),
+                    today_local_date_time(),
+                    today_offset_date_time(),
+                    "3.14",
+                    "42",
+                    "[]",
+                    "{}",
+                    "$key",
+                    "true",
+                    "false",
+                ]);
+            }
+        };
 
-    (
-        #[tokio::test]
-        async fn _$name:ident(
-            $source:expr,
-            $schema_file_path:expr$(,)?
-        ) -> Ok([$($label:expr),*$(,)?]);
-    ) => {
-        #[tokio::test]
-        async fn $name() -> Result<(), Box<dyn std::error::Error>> {
-            use itertools::Itertools;
-            use server::Backend;
-            use std::io::Write;
-            use tower_lsp::{
-                lsp_types::{
-                    DidOpenTextDocumentParams, PartialResultParams, TextDocumentIdentifier,
-                    TextDocumentItem, Url, WorkDoneProgressParams, CompletionItem,
-                    CompletionParams, TextDocumentPositionParams
-                },
-                LspService,
-            };
-            use server::handler::handle_did_open;
+        (
+            #[tokio::test]
+            async fn _$name:ident(
+                $source:expr,
+                $schema_file_path:expr$(,)?
+            ) -> Ok([$($label:expr),*$(,)?]);
+        ) => {
+            #[tokio::test]
+            async fn $name() -> Result<(), Box<dyn std::error::Error>> {
+                use itertools::Itertools;
+                use server::Backend;
+                use std::io::Write;
+                use tower_lsp::{
+                    lsp_types::{
+                        CompletionItem, CompletionParams, DidOpenTextDocumentParams,
+                        PartialResultParams, TextDocumentIdentifier, TextDocumentItem,
+                        TextDocumentPositionParams, Url, WorkDoneProgressParams,
+                    },
+                    LspService,
+                };
+                use server::handler::handle_did_open;
 
-            if let Ok(level) = std::env::var("RUST_LOG") {
+                if let Ok(level) = std::env::var("RUST_LOG") {
                     let _ = tracing_subscriber::fmt()
                         .with_env_filter(level)
                         .pretty()
                         .try_init();
-            }
+                }
 
-            let (service, _) = LspService::new(|client| Backend::new(client));
+                let (service, _) = LspService::new(|client| Backend::new(client));
+                let backend = service.inner();
 
-            let backend = service.inner();
+                if let Some(schema_file_path) = $schema_file_path.as_ref() {
+                    let schema_url = schema_store::SchemaUrl::from_file_path(schema_file_path)
+                        .expect(
+                            format!(
+                                "failed to convert schema path to URL: {}",
+                                schema_file_path.display()
+                            )
+                            .as_str(),
+                        );
+                    backend
+                        .schema_store
+                        .add_json_schema(schema_store::json::JsonSchema {
+                            name: "test_schema".to_string(),
+                            description: "schema for testing".to_string(),
+                            file_match: vec!["*.toml".to_string()],
+                            url: schema_url.clone(),
+                        })
+                        .await;
+                }
 
-            if let Some(schema_file_path) = $schema_file_path.as_ref() {
-                let schema_url = schema_store::SchemaUrl::from_file_path(schema_file_path).expect(
-                    format!(
-                        "failed to convert schema path to URL: {}",
-                        schema_file_path.display()
-                    )
-                    .as_str(),
-                );
-                backend
-                    .schema_store
-                    .add_json_schema(schema_store::json::JsonSchema {
-                        name: "test_schema".to_string(),
-                        description: "schema for testing".to_string(),
-                        file_match: vec!["*.toml".to_string()],
-                        url: schema_url.clone(),
-                    })
-                    .await;
-            }
-
-            let Ok(temp_file) = tempfile::NamedTempFile::with_suffix_in(
-                ".toml",
-                std::env::current_dir().expect("failed to get current directory"),
-            ) else {
-                return Err("failed to create a temporary file for the test data".into());
-            };
-
-            let mut toml_text = textwrap::dedent($source).trim().to_string();
-
-            let Some(index) = toml_text
-                .as_str()
-                .find("█")
-                else {
-                    return Err("failed to find completion position marker (█) in the test data".into());
+                let Ok(temp_file) = tempfile::NamedTempFile::with_suffix_in(
+                    ".toml",
+                    std::env::current_dir().expect("failed to get current directory"),
+                ) else {
+                    return Err("failed to create a temporary file for the test data".into());
                 };
 
-            toml_text.remove(index);
-            if temp_file.as_file().write_all(toml_text.as_bytes()).is_err() {
-                return  Err("failed to write test data to the temporary file, which is used as a text document".into());
-            };
+                let mut toml_text = textwrap::dedent($source).trim().to_string();
 
-            let Ok(toml_file_url) = Url::from_file_path(temp_file.path()) else {
-                return Err("failed to convert temporary file path to URL".into());
-            };
+                let Some(index) = toml_text.as_str().find("█") else {
+                    return Err(
+                        "failed to find completion position marker (█) in the test data".into()
+                    );
+                };
 
-            handle_did_open(
-                backend,
-                DidOpenTextDocumentParams {
-                    text_document: TextDocumentItem {
-                        uri: toml_file_url.clone(),
-                        language_id: "toml".to_string(),
-                        version: 0,
-                        text: toml_text.clone(),
+                toml_text.remove(index);
+                if temp_file.as_file().write_all(toml_text.as_bytes()).is_err() {
+                    return Err(
+                        "failed to write test data to the temporary file, which is used as a text document"
+                            .into(),
+                    );
+                };
+
+                let Ok(toml_file_url) = Url::from_file_path(temp_file.path()) else {
+                    return Err("failed to convert temporary file path to URL".into());
+                };
+
+                handle_did_open(
+                    backend,
+                    DidOpenTextDocumentParams {
+                        text_document: TextDocumentItem {
+                            uri: toml_file_url.clone(),
+                            language_id: "toml".to_string(),
+                            version: 0,
+                            text: toml_text.clone(),
+                        },
                     },
-                },
-            )
-            .await;
-
-            let Ok(Some(completions)) = server::handler::handle_completion(
-                &backend,
-                CompletionParams {
-                    text_document_position: TextDocumentPositionParams {
-                        text_document: TextDocumentIdentifier { uri: toml_file_url },
-                        position: (text::Position::default()
-                            + text::RelativePosition::of(&toml_text[..index]))
-                        .into(),
-                    },
-                    work_done_progress_params: WorkDoneProgressParams::default(),
-                    partial_result_params: PartialResultParams {
-                        partial_result_token: None,
-                    },
-                    context: None,
-                },
-            )
-            .await else {
-                return Err("failed to handle completion".into());
-            };
-
-            let labels = completions
-                .into_iter()
-                .map(|content| Into::<CompletionItem>::into(content))
-                .sorted_by(|a, b|
-                    a.sort_text.as_ref().unwrap_or(&a.label).cmp(&b.sort_text.as_ref().unwrap_or(&b.label))
                 )
-                .map(|item| item.label)
-                .collect::<Vec<_>>();
+                .await;
 
-            pretty_assertions::assert_eq!(
-                labels,
-                vec![$($label.to_string()),*] as Vec<String>,
-            );
+                let Ok(Some(completions)) = server::handler::handle_completion(
+                    &backend,
+                    CompletionParams {
+                        text_document_position: TextDocumentPositionParams {
+                            text_document: TextDocumentIdentifier {
+                                uri: toml_file_url,
+                            },
+                            position: (text::Position::default()
+                                + text::RelativePosition::of(&toml_text[..index]))
+                            .into(),
+                        },
+                        work_done_progress_params: WorkDoneProgressParams::default(),
+                        partial_result_params: PartialResultParams {
+                            partial_result_token: None,
+                        },
+                        context: None,
+                    },
+                )
+                .await
+                else {
+                    return Err("failed to handle completion".into());
+                };
 
-            Ok(())
-        }
-    };
+                let labels = completions
+                    .into_iter()
+                    .map(|content| Into::<CompletionItem>::into(content))
+                    .sorted_by(|a, b| {
+                        a.sort_text
+                            .as_ref()
+                            .unwrap_or(&a.label)
+                            .cmp(&b.sort_text.as_ref().unwrap_or(&b.label))
+                    })
+                    .map(|item| item.label)
+                    .collect::<Vec<_>>();
+
+                pretty_assertions::assert_eq!(
+                    labels,
+                    vec![$($label.to_string()),*] as Vec<String>,
+                );
+
+                Ok(())
+            }
+        };
+    }
 }
