489ff36e0c37c4c1195717311b0660c67d1c5012
yassun7010 <yassun7010@outlook.com>
2025-03-11 01:34:33 +0900

    refactor: x-tombi

--- crates/x-tombi/src/lib.rs
+++ crates/x-tombi/src/lib.rs
@@ -1,9 +1,41 @@
-mod array_values_order;
-mod table_keys_order;
-
 pub const X_TOMBI_TOML_VERSION: &str = "x-tombi-toml-version";
 pub const X_TOMBI_ARRAY_VALUES_ORDER: &str = "x-tombi-array-values-order";
 pub const X_TOMBI_TABLE_KEYS_ORDER: &str = "x-tombi-table-keys-order";
 
-pub use array_values_order::ArrayValuesOrder;
-pub use table_keys_order::TableKeysOrder;
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+#[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
+pub enum ArrayValuesOrder {
+    Ascending,
+    Descending,
+}
+
+impl std::fmt::Display for ArrayValuesOrder {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Ascending => write!(f, "ascending"),
+            Self::Descending => write!(f, "descending"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+#[cfg_attr(feature = "jsonschema", derive(schemars::JsonSchema))]
+pub enum TableKeysOrder {
+    Ascending,
+    Descending,
+    Schema,
+}
+
+impl std::fmt::Display for TableKeysOrder {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TableKeysOrder::Ascending => write!(f, "ascending"),
+            TableKeysOrder::Descending => write!(f, "descending"),
+            TableKeysOrder::Schema => write!(f, "schema"),
+        }
+    }
+}
