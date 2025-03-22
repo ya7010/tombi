49486b7f2b7e025b7b55e8ca6930e506332aa1f9
yassun7010 <yassun7010@outlook.com>
2025-03-22 14:15:38 +0900

    chore: update modifiers.

--- crates/server/src/handler/initialize.rs
+++ crates/server/src/handler/initialize.rs
@@ -84,10 +84,7 @@ pub fn server_capabilities(client_capabilities: &ClientCapabilities) -> ServerCa
             SemanticTokensOptions {
                 legend: SemanticTokensLegend {
                     token_types: SUPPORTED_TOKEN_TYPES.to_vec(),
-                    token_modifiers: vec![
-                        SemanticTokenModifier::DECLARATION,
-                        SemanticTokenModifier::READONLY,
-                    ],
+                    token_modifiers: vec![],
                 },
                 full: Some(SemanticTokensFullOptions::Bool(true)),
                 ..Default::default()
