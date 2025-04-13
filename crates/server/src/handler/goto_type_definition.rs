05f13db2d9f9d41cd196c2918b0a5211bd510324
yassun7010 <yassun7010@outlook.com>
2025-04-14 03:08:02 +0900

    feat: implemented goto type defitition.

--- crates/server/src/handler/goto_type_definition.rs
+++ crates/server/src/handler/goto_type_definition.rs
@@ -1,7 +1,12 @@
+use document_tree::IntoDocumentTreeAndErrors;
 use itertools::Either;
-use tower_lsp::lsp_types::request::{GotoTypeDefinitionParams, GotoTypeDefinitionResponse};
+use schema_store::SchemaContext;
+use tower_lsp::lsp_types::{
+    request::{GotoTypeDefinitionParams, GotoTypeDefinitionResponse},
+    Location, LocationLink,
+};
 
-use crate::backend::Backend;
+use crate::{backend::Backend, handler::hover::get_hover_range, hover::get_hover_content};
 
 #[tracing::instrument(level = "debug", skip_all)]
 pub async fn handle_goto_type_definition(
@@ -13,7 +18,11 @@ pub async fn handle_goto_type_definition(
 
     let GotoTypeDefinitionParams {
         text_document_position_params:
-            tower_lsp::lsp_types::TextDocumentPositionParams { text_document, .. },
+            tower_lsp::lsp_types::TextDocumentPositionParams {
+                text_document,
+                position,
+                ..
+            },
         ..
     } = params;
 
@@ -30,16 +39,48 @@ pub async fn handle_goto_type_definition(
         return Ok(None);
     }
 
+    let position = position.into();
     let Some(root) = backend.get_incomplete_ast(&text_document.uri).await else {
         return Ok(None);
     };
 
-    let _source_schema = backend
+    let source_schema = backend
         .schema_store
         .try_get_source_schema_from_ast(&root, Some(Either::Left(&text_document.uri)))
         .await
         .ok()
         .flatten();
 
-    Ok(None)
+    let (toml_version, _) = backend.source_toml_version(source_schema.as_ref()).await;
+
+    let Some((keys, range)) = get_hover_range(&root, position, toml_version).await else {
+        return Ok(None);
+    };
+
+    if keys.is_empty() && range.is_none() {
+        return Ok(None);
+    }
+
+    let document_tree = root.into_document_tree_and_errors(toml_version).tree;
+
+    Ok(get_hover_content(
+        &document_tree,
+        position,
+        &keys,
+        &SchemaContext {
+            toml_version,
+            root_schema: source_schema.as_ref().and_then(|s| s.root_schema.as_ref()),
+            sub_schema_url_map: source_schema.as_ref().map(|s| &s.sub_schema_url_map),
+            store: &backend.schema_store,
+        },
+    )
+    .await
+    .and_then(|content| {
+        content.schema_url.map(|schema_url| {
+            GotoTypeDefinitionResponse::Scalar(Location {
+                uri: schema_url.into(),
+                range: text::Range::default().into(),
+            })
+        })
+    }))
 }
