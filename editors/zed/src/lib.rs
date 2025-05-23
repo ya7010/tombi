878eb8bf165206e0e308e7c9901184d06084670c
yassun7010 <yassun7010@outlook.com>
2025-05-24 00:24:38 +0900

    fix: zed windows.

--- editors/zed/src/lib.rs
+++ editors/zed/src/lib.rs
@@ -96,7 +96,7 @@ impl TombiExtension {
         fs::create_dir_all(&version_dir)
             .map_err(|err| format!("failed to create directory '{version_dir}': {err}"))?;
         let binary_path = match platform {
-            zed::Os::Windows => format!("{version_dir}/tombi.exe"),
+            zed::Os::Windows => format!("{version_dir}"),
             _ => format!("{version_dir}/tombi"),
         };
 
