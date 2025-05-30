7d145a19c5a3d0fb0553867a312e9f2a50ee25cf
ya7010 <ya7010@outlook.com>
2025-05-31 00:43:28 +0900

    feat: add handling for first member pyproject.toml links
    - Introduced a new tooltip for the first member's pyproject.toml.
    - Enhanced document link functionality to support workspace members.

--- extensions/tombi-extension-uv/src/document_link.rs
+++ extensions/tombi-extension-uv/src/document_link.rs
@@ -2,10 +2,11 @@ use tombi_config::TomlVersion;
 use tombi_document_tree::dig_keys;
 use tower_lsp::lsp_types::{TextDocumentIdentifier, Url};
 
-use crate::{find_member_project_toml, find_workspace_pyproject_toml};
+use crate::{find_member_project_toml, find_workspace_pyproject_toml, goto_member_pyprojects};
 
 pub enum DocumentLinkToolTip {
     PyprojectToml,
+    PyprojectTomlFirstMember,
     WorkspacePyprojectToml,
 }
 
@@ -13,6 +14,9 @@ impl std::fmt::Display for DocumentLinkToolTip {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             DocumentLinkToolTip::PyprojectToml => write!(f, "Open pyproject.toml"),
+            DocumentLinkToolTip::PyprojectTomlFirstMember => {
+                write!(f, "Open first pyproject.toml in members")
+            }
             DocumentLinkToolTip::WorkspacePyprojectToml => {
                 write!(f, "Open Workspace pyproject.toml")
             }
@@ -36,7 +40,16 @@ pub async fn document_link(
 
     let mut document_links = vec![];
 
-    if let Some((_, tombi_document_tree::Value::Table(sources))) =
+    if let Some((_, tombi_document_tree::Value::Table(workspace))) =
+        dig_keys(document_tree, &["tool", "uv", "workspace"])
+    {
+        document_links.extend(document_link_for_workspace_pyproject_toml(
+            document_tree,
+            workspace,
+            &pyproject_toml_path,
+            toml_version,
+        )?);
+    } else if let Some((_, tombi_document_tree::Value::Table(sources))) =
         dig_keys(document_tree, &["tool", "uv", "sources"])
     {
         for (package_name_key, source) in sources.key_values() {
@@ -54,6 +67,67 @@ pub async fn document_link(
     }
 
     Ok(Some(document_links))
+}
+
+fn document_link_for_workspace_pyproject_toml(
+    workspace_document_tree: &tombi_document_tree::DocumentTree,
+    workspace: &tombi_document_tree::Table,
+    workspace_pyproject_toml_path: &std::path::Path,
+    toml_version: TomlVersion,
+) -> Result<Vec<tombi_extension::DocumentLink>, tower_lsp::jsonrpc::Error> {
+    let Some(tombi_document_tree::Value::Array(members)) = workspace.get("members") else {
+        return Ok(Vec::with_capacity(0));
+    };
+
+    let mut total_document_links = vec![];
+    for (i, member) in members.values().iter().enumerate() {
+        let tombi_document_tree::Value::String(member) = member else {
+            continue;
+        };
+
+        let Ok(member_paskage_locations) = goto_member_pyprojects(
+            workspace_document_tree,
+            &[
+                tombi_schema_store::Accessor::Key("tool".to_string()),
+                tombi_schema_store::Accessor::Key("uv".to_string()),
+                tombi_schema_store::Accessor::Key("workspace".to_string()),
+                tombi_schema_store::Accessor::Key("members".to_string()),
+                tombi_schema_store::Accessor::Index(i),
+            ],
+            workspace_pyproject_toml_path,
+            toml_version,
+        ) else {
+            continue;
+        };
+
+        let mut member_document_links =
+            member_paskage_locations.into_iter().filter_map(|location| {
+                let Ok(member_pyproject_toml_url) =
+                    Url::from_file_path(&location.pyproject_toml_path)
+                else {
+                    return None;
+                };
+                Some(tombi_extension::DocumentLink {
+                    target: member_pyproject_toml_url,
+                    range: member.unquoted_range(),
+                    tooltip: DocumentLinkToolTip::PyprojectTomlFirstMember.to_string(),
+                })
+            });
+
+        match member_document_links.size_hint() {
+            (_, Some(n)) if n > 0 => {
+                if let Some(mut document_link) = member_document_links.next() {
+                    if n == 1 {
+                        document_link.tooltip = DocumentLinkToolTip::PyprojectToml.to_string();
+                    }
+                    total_document_links.push(document_link);
+                }
+            }
+            _ => {}
+        }
+    }
+
+    Ok(total_document_links)
 }
 
 fn document_link_for_member_pyproject_toml(
