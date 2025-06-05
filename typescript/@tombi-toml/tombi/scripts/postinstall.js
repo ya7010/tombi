21c0c15cf6084ad5d1ec57917fade937ab87f8fc
ya7010 <ya7010@outlook.com>
2025-06-06 03:55:16 +0900

    chore: update CLI references in postinstall script
    Updated the postinstall script to replace "Biome" with "Tombi" in warning messages for clarity regarding the CLI package.

--- typescript/@tombi-toml/tombi/scripts/postinstall.js
+++ typescript/@tombi-toml/tombi/scripts/postinstall.js
@@ -47,12 +47,12 @@ if (binName) {
 		binPath = require.resolve(binName);
 	} catch {
 		console.warn(
-			`The Biome CLI postinstall script failed to resolve the binary file "${binName}". Running Biome from the npm package will probably not work correctly.`,
+			`The Tombi CLI postinstall script failed to resolve the binary file "${binName}". Running Tombi from the npm package will probably not work correctly.`,
 		);
 	}
 } else {
 	console.warn(
-		"The Biome CLI package doesn't ship with prebuilt binaries for your platform yet. " +
+		"The Tombi CLI package doesn't ship with prebuilt binaries for your platform yet. " +
 			"You can still use the CLI by cloning the tombi-toml/tombi repo from GitHub, " +
 			"and follow the instructions there to build the CLI for your platform.",
 	);
