45ac04ebd7d907d3faa6aa6aab762f334e7143fa
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-06-09 23:35:04 -0700

    Clean up flake and use crane to actually make the binary accessible

--- flake.nix
+++ flake.nix
@@ -3,47 +3,67 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+    };
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
     };
-
-    wild = {
-      url = "github:davidlattimore/wild";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     {
       self,
+      crane,
+      flake-utils,
       nixpkgs,
       rust-overlay,
-      wild,
     }:
-    let
-      system = "x86_64-linux";
-      overlays = [
-        (import rust-overlay)
-        wild.overlays.default
-      ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
 
-      wildStdenv = pkgs.useWildLinker pkgs.stdenv;
-    in
-    with pkgs;
-    {
-      devShells.${system}.default = mkShell.override { stdenv = wildStdenv; } {
-        buildInputs = [
-          (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-          clang
-          openssl
-          pkg-config
-        ];
-      };
-    };
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+        );
+
+        tombiPkg = craneLib.buildPackage {
+          pname = "tombi";
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+          doCheck = true;
+          doNotSign = false;
+        };
+      in
+      {
+        checks = {
+          inherit tombiPkg;
+        };
+
+        packages.default = tombiPkg;
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks
+          checks = self.checks.${system};
+
+          packages = with pkgs; [
+            openssl
+            pkg-config
+          ];
+        };
+      }
+    );
 }
